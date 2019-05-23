d926552aa09b4dcab34fa88b41b1f30b83534463
VVerkest <vverkest@gmail.com>
2019-05-22 20:35:27 -0400

    change bins

--- src/pAuPlot.C
+++ src/pAuPlot.C
@@ -6,7 +6,7 @@ void pAuPlot() {
   TH1::SetDefaultSumw2();  TH2::SetDefaultSumw2();  TH3::SetDefaultSumw2();
   TH3D *hVertex = new TH3D( "hVertex", "Event Vertex;v_{x};v_{y};v_{z}", 60,-0.3,0.3, 60,-0.3,0.3, 160,-40,40 );
   TH2D *hTowersPerEvent = new TH2D("hTowersPerEvent","Tower Multiplicity (per event);Event no.;# of Towers", 15000,0,1500000, 700,0,700 );
-  TH2D *hTowersPerRun = new TH2D("hTowersPerRun","Tower Multiplicity (per run);Run no.;# of Towers", 15000,2000000,17000000, 700,0,700 );
+  TH2D *hTowersPerRun = new TH2D("hTowersPerRun","Tower Multiplicity (per run);Run no.;# of Towers", 20000,16000000,16200000, 700,0,700 );
   // 2119440-16156005      2000000,17000000
   const float pi = 3.141592;
 
@@ -42,22 +42,22 @@ void pAuPlot() {
     hTowersPerRun->Fill( RunID, nTowers );
   }
 
-  TCanvas * c0 = new TCanvas( "c0" , "" ,0 ,23 ,1280 ,700 );
-  hVertex->Draw();
+  // TCanvas * c0 = new TCanvas( "c0" , "" ,0 ,23 ,1280 ,700 );
+  // hVertex->Draw();
 
-  TCanvas * c1 = new TCanvas( "c1" , "" ,0 ,23 ,1280 ,700 );
-  hVertex->Project3D("XY")->Draw("colz");
+  // TCanvas * c1 = new TCanvas( "c1" , "" ,0 ,23 ,1280 ,700 );
+  // hVertex->Project3D("XY")->Draw("colz");
 
-  TCanvas * c2 = new TCanvas( "c2" , "" ,0 ,23 ,1280 ,700 );
-  hVertex->Project3D("YZ")->Draw("colz");
+  // TCanvas * c2 = new TCanvas( "c2" , "" ,0 ,23 ,1280 ,700 );
+  // hVertex->Project3D("YZ")->Draw("colz");
 
-  TCanvas * c3 = new TCanvas( "c3" , "" ,0 ,23 ,1280 ,700 );
-  hVertex->Project3D("XZ")->Draw("colz");
+  // TCanvas * c3 = new TCanvas( "c3" , "" ,0 ,23 ,1280 ,700 );
+  // hVertex->Project3D("XZ")->Draw("colz");
 
   TCanvas * c4 = new TCanvas( "c4" , "" ,0 ,23 ,1280 ,700 );
-  hTowersPerEvent->Draw();
+  hTowersPerEvent->Draw("COLZ");
   
   TCanvas * c5 = new TCanvas( "c5" , "" ,0 ,23 ,1280 ,700 );
-  hTowersPerRun->Draw();
+  hTowersPerRun->Draw("COLZ");
 
 }
