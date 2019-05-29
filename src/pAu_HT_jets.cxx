7404492a260bd721ef4d227359a324216e90f5ff
VVerkest <vverkest@gmail.com>
2019-05-29 16:12:26 -0400

    change to vectors

--- src/pAu_HT_jets.cxx
+++ src/pAu_HT_jets.cxx
@@ -43,13 +43,14 @@ int main ( int argc, const char** argv ) {
   
   TFile *pAuFile = new TFile( outFile.c_str() ,"RECREATE");
   
-
+  double dca, pt, pz, eta, phi;
+  
   TTree *HTjetTree = new TTree( "HTjetTree", "HT_JetTree" );
   
-  int RunID, EventID, nTowers, nPrimary, nGlobal, nVertices, refMult, gRefMult, nHitsPoss, nHitsFit, nJets;
-  double Vx, Vy, Vz, DCA, BbcCoincidenceRate, BbcEastRate, BbcWestRate, vpdVz;
-  vector<int> towID, Charge, nCons, leadNcons, subNcons;
-  vector<double> towEt, towEta, towPhi, trEta, trPhi, trPz, trPt, jetPt, jetEta, jetPhi, jetEt, leadPt, leadEta, leadPhi, leadEt, subPt, subEta, subPhi, subEt;
+  int RunID, EventID, nTowers, nPrimary, nGlobal, nVertices, refMult, gRefMult, nHitsPoss, nHitsFit, nJets, leadNcons, subNcons;
+  double Vx, Vy, Vz, DCA, BbcCoincidenceRate, BbcEastRate, BbcWestRate, vpdVz,  leadPt, leadEta, leadPhi, leadEt, subPt, subEta, subPhi, subEt;
+  vector<int> towID, Charge, nCons;
+  vector<double> towEt, towEta, towPhi, trEta, trPhi, trPz, trPt, jetPt, jetEta, jetPhi, jetEt;
 
   HTjetTree->Branch("EventID", &EventID);            HTjetTree->Branch("RunID", &RunID);                  HTjetTree->Branch("Vz", &Vz);
   HTjetTree->Branch("nTowers", &nTowers);          HTjetTree->Branch("nPrimary", &nPrimary);         HTjetTree->Branch("nGlobal", &nGlobal);
@@ -58,7 +59,7 @@ int main ( int argc, const char** argv ) {
   HTjetTree->Branch("BbcWestRate", &BbcWestRate);                                           HTjetTree->Branch("vpdVz", &vpdVz);
   HTjetTree->Branch("towEt", &towEt);                   HTjetTree->Branch("towEta", &towEta);	          HTjetTree->Branch("towPhi", &towPhi);
   HTjetTree->Branch("towID", &towID);                  HTjetTree->Branch("nHitsPoss", &nHitsPoss);     HTjetTree->Branch("nHitsFit", &nHitsFit);
-  HTjetTree->Branch("trPx", &trPx);                        HTjetTree->Branch("trPy", &trPy);                        HTjetTree->Branch("trPz", &trPz);
+  HTjetTree->Branch("trPz", &trPz);
   HTjetTree->Branch("trPt", &trPt);                          HTjetTree->Branch("trEta", &trEta);                     HTjetTree->Branch("trPhi", &trPhi);
   HTjetTree->Branch("DCA", &DCA);                       HTjetTree->Branch("nJets", &nJets);                    HTjetTree->Branch("jetPt", &jetPt);
   HTjetTree->Branch("jetEta", &jetEta);                    HTjetTree->Branch("jetPhi", &jetPhi);                       HTjetTree->Branch("jetEt", &jetEt);
@@ -77,12 +78,14 @@ int main ( int argc, const char** argv ) {
   
   TStarJetPicoEventHeader* header;    TStarJetPicoEvent* event;    TStarJetVector* sv;
   TStarJetVectorContainer<TStarJetVector> * container;
-
   
   // ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~  BEGIN EVENT LOOP!  ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
   while ( Reader.NextEvent() ) {
 
-    rawParticles.clear();  rawJets.clear();  nCons.clear();    jetPt.clear();    jetEta.clear();    jetPhi.clear();    jetEt.clear();  //  clear vectors
+    rawParticles.clear();     rawJets.clear();     nCons.clear();     jetPt.clear();     jetEta.clear();     jetPhi.clear();     jetEt.clear();     towID.clear();     Charge.clear();
+    nCons.clear();     leadNcons.clear();     subNcons.clear();     towEt.clear();     towEta.clear();     towPhi.clear();     trEta.clear();     trPhi.clear();     trPz.clear();
+    trPt.clear();     jetPt.clear();     jetEta.clear();     jetPhi.clear();     jetEt.clear();     leadPt.clear();     leadEta.clear();     leadPhi.clear();     leadEt.clear();
+    subPt.clear();     subEta.clear();     subPhi.clear();     subEt.clear();       //  clear vectors
 
     Reader.PrintStatus(5);
 
@@ -105,91 +108,92 @@ int main ( int argc, const char** argv ) {
     refMult = header->GetReferenceMultiplicity();                           gRefMult = header->GetGReferenceMultiplicity();
     BbcCoincidenceRate = header->GetBbcCoincidenceRate();        vpdVz = header->GetVpdVz();
     BbcEastRate = header->GetBbcEastRate();                                  BbcWestRate = header->GetBbcWestRate();
-
     
     for ( int i=0; i<npt; ++i ) {                                         //  FILL EVENT DATA
 
-      DCA = event->GetPrimaryTrack(i)->GetDCA();
-      if ( DCA > dcaCut ) { nPrimary -= 1;   continue; }                   // track DCA cut
-      trEta = (double) event->GetPrimaryTrack(i)->GetEta();
-      if ( abs(trEta) > etaCut ) { nPrimary -= 1;   continue; }            // track eta cut
-
+      dca = event->GetPrimaryTrack(i)->GetDCA();
+      if ( dca > dcaCut ) { nPrimary -= 1;   continue; }                   // track DCA cut
+      DCA.push_back( dca );
+      eta = (double) event->GetPrimaryTrack(i)->GetEta();
+      if ( abs(eta) > etaCut ) { nPrimary -= 1;   continue; }            // track eta cut
+      trEta.push_back(eta);
       trPt.push_back((double) event->GetPrimaryTrack(i)->GetPt());
-      trPz = (double) event->GetPrimaryTrack(i)->GetPz();
-      trPhi = (double) event->GetPrimaryTrack(i)->GetPhi();
+      trPz.push_back((double) event->GetPrimaryTrack(i)->GetPz());
+      trPhi.push_back((double) event->GetPrimaryTrack(i)->GetPhi());
 
-      nHitsFit = event->GetPrimaryTrack(i)->GetNOfFittedHits();
-      nHitsPoss = event->GetPrimaryTrack(i)->GetNOfPossHits();
+      nHitsFit.push_back(event->GetPrimaryTrack(i)->GetNOfFittedHits());
+      nHitsPoss.push_back(event->GetPrimaryTrack(i)->GetNOfPossHits());
     }
 
     
     for ( int i=0; i<ntow; ++i ) {                                         //  FILL TOWER DATA
-      towEt = event->GetTower(i)->GetEt();
-      towEta = event->GetTower(i)->GetEta();
-      if ( abs(towEta) > etaCut ) { nTowers -= 1;   continue; }            // tower eta cut
-      towPhi = event->GetTower(i)->GetPhi();
-      towID = event->GetTower(i)->GetId();
-    }
-
-
-
-    //   JET-FINDING
-    GatherParticles( container, rawParticles);     //  GATHERS ALL PARTICLES WITH    pT>=2.0GeV    and    |eta|<1.0
-
-    ClusterSequence jetCluster( rawParticles, jet_def );           //  CLUSTER ALL JETS
-    vector<PseudoJet> rawJets = sorted_by_pt( etaPtSelector( jetCluster.inclusive_jets() ) );     // EXTRACT SELECTED JETS
-
-    // nJets=0;
-    
-    // for ( int i=0; i<rawJets.size(); ++i ) {                              //  FILL JET INFO
-    //   if ( rawJets[i].pt()<jetMinPt )  { cerr<<"bad jet pt selector?"<<endl;   continue; }
-    //   jetPt.push_back( rawJets[i].pt() );
-    //   if ( abs(rawJets[i].eta())>etaCut ) { cerr<<"bad jet eta selector?"<<endl;   continue; }
-    //   jetEta.push_back( rawJets[i].eta() );
-    //   jetPhi.push_back( rawJets[i].phi() );
-    //   jetEt.push_back( rawJets[i].Et() );
-    //   vector<PseudoJet> Cons= rawJets[i].constituents();
-    //   nCons.push_back( Cons.size() );
-    //   nJets+=1;
-    // }
-
-
-    // if ( rawJets.size()>=2 ) {                        //  CREATE DIJET PAIR
-    //   double dphi = fabs( fabs( rawJets.at(0).delta_phi_to( rawJets.at(1) ) ) - pi );
-    //   if ( dphi< R && rawJets[0].pt()>2.0 && rawJets[1].pt()>2.0 ) {  
-    // 	leadPt = rawJets[0].pt();
-    // 	leadEta = rawJets[0].eta();
-    // 	leadPhi = rawJets[0].phi();
-    // 	leadEt = rawJets[0].Et();
-    // 	vector<PseudoJet> LeadCons= rawJets[0].constituents();
-    // 	leadNcons = LeadCons.size();
-    // 	subPt = rawJets[0].pt();
-    // 	subEta = rawJets[0].eta();
-    // 	subPhi = rawJets[0].phi();
-    // 	subEt = rawJets[0].Et();
-    // 	vector<PseudoJet> SubCons= rawJets[1].constituents();
-    // 	subNcons = SubCons.size();
-    //   }
-    // }
-
-    // double djAxisPhi = ( rawJets[0].phi() + rawJets[1].phi() )/2;
-    // Selector bgCircle = SelectorCircle(R);
-    // Selector bgRapRange = SelectorRapRange( -0.6, 0.6 );
-    // Selector bgSelector = bgCircle && bgRapRange;
-    // double ghost_maxrap = 1.0;
-    // AreaDefinition area_def(active_area, GhostedAreaSpec(ghost_maxrap));
-    // JetMedianBackgroundEstimator UE( bgSelector, jet_def, area_def);
-    
-    HTjetTree->Fill();                                                           //  FILL TREE
+      towEt.push_back(event->GetTower(i)->GetEt());
+      eta = event->GetTower(i)->GetEta());
+    if ( abs(eta) > etaCut ) { nTowers -= 1;   continue; }            // tower eta cut
+    towEta.push_back(eta);
+    towPhi.push_back(event->GetTower(i)->GetPhi());
+    towID.push_back(event->GetTower(i)->GetId());
   }
-  // ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~  END EVENT LOOP!  ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+
+
+
+  //   JET-FINDING
+  GatherParticles( container, rawParticles);     //  GATHERS ALL PARTICLES WITH    pT>=2.0GeV    and    |eta|<1.0
+
+  ClusterSequence jetCluster( rawParticles, jet_def );           //  CLUSTER ALL JETS
+  vector<PseudoJet> rawJets = sorted_by_pt( etaPtSelector( jetCluster.inclusive_jets() ) );     // EXTRACT SELECTED JETS
+
+  nJets=0;
+    
+  for ( int i=0; i<rawJets.size(); ++i ) {                              //  FILL JET INFO
+    if ( rawJets[i].pt()<jetMinPt )  { cerr<<"bad jet pt selector?"<<endl;   continue; }
+    jetPt.push_back( rawJets[i].pt() );
+    if ( abs(rawJets[i].eta())>etaCut ) { cerr<<"bad jet eta selector?"<<endl;   continue; }
+    jetEta.push_back( rawJets[i].eta() );
+    jetPhi.push_back( rawJets[i].phi() );
+    jetEt.push_back( rawJets[i].Et() );
+    vector<PseudoJet> Cons= rawJets[i].constituents();
+    nCons.push_back( Cons.size() );
+    nJets+=1;
+  }
+
+
+  if ( rawJets.size()>=2 ) {                        //  CREATE DIJET PAIR
+    double dphi = fabs( fabs( rawJets.at(0).delta_phi_to( rawJets.at(1) ) ) - pi );
+    if ( dphi< R && rawJets[0].pt()>2.0 && rawJets[1].pt()>2.0 ) {  
+      leadPt = rawJets[0].pt();
+      leadEta = rawJets[0].eta();
+      leadPhi = rawJets[0].phi();
+      leadEt = rawJets[0].Et();
+      vector<PseudoJet> LeadCons= rawJets[0].constituents();
+      leadNcons = LeadCons.size();
+      subPt = rawJets[0].pt();
+      subEta = rawJets[0].eta();
+      subPhi = rawJets[0].phi();
+      subEt = rawJets[0].Et();
+      vector<PseudoJet> SubCons= rawJets[1].constituents();
+      subNcons = SubCons.size();
+    }
+  }
+
+  // double djAxisPhi = ( rawJets[0].phi() + rawJets[1].phi() )/2;
+  // Selector bgCircle = SelectorCircle(R);
+  // Selector bgRapRange = SelectorRapRange( -0.6, 0.6 );
+  // Selector bgSelector = bgCircle && bgRapRange;
+  // double ghost_maxrap = 1.0;
+  // AreaDefinition area_def(active_area, GhostedAreaSpec(ghost_maxrap));
+  // JetMedianBackgroundEstimator UE( bgSelector, jet_def, area_def);
+    
+  HTjetTree->Fill();                                                           //  FILL TREE
+}
+// ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~  END EVENT LOOP!  ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
 
     
-  HTjetTree->Write();                            //  WRITE TREES
+HTjetTree->Write();                            //  WRITE TREES
 
 
-  pAuFile->Close();
+pAuFile->Close();
   
   
-  return 0;
+return 0;
 }
