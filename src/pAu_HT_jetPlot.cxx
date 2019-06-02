91979577d2388671b280e6bebc4159e879395ddd
VVerkest <vverkest@gmail.com>
2019-06-02 19:27:00 -0400

    add plots, fix errors

--- src/pAu_HT_jetPlot.cxx
+++ src/pAu_HT_jetPlot.cxx
@@ -33,13 +33,14 @@ int main() {
 
   TH2D *hPrimaryVsBBCE = new TH2D("hPrimaryVsBBCE","# Primary Tracks vs. BBC East Rate;BBC East Rate;# Primary Tracks", 50000,0,5000000, 150,0,150 );
   TH2D *hGlobalVsBBCE = new TH2D("hGlobalVsBBCE","# Global Tracks vs. BBC East Rate;BBC East Rate;# Global Tracks", 50000,0,5000000, 300,0,3000 );
-  TH2D *hLeadEtaPhi = new TH2D("hLeadEtaPhi","Lead Jet #eta vs. #phi;#phi;#eta", 128,-3.2,3.2, 100,-1.0,1.0);
-  TH2D *hSubEtaPhi = new TH2D("hSubEtaPhi","Sub Jet #eta vs. #phi;#phi;#eta", 128,-3.2,3.2, 100,-1.0,1.0);
+  TH2D *hPrimaryVsGlobal = new TH2D("hPrimaryVsGlobal","# Primary Tracks vs. # Global Tracks;# Global Tracks;# Primary Tracks", 300,0,3000, 150,0,150 );
+  TH2D *hLeadEtaPhi = new TH2D("hLeadEtaPhi","Lead Jet #eta vs. #phi;#phi;#eta", 126,0,6.3, 100,-1.0,1.0);
+  TH2D *hSubEtaPhi = new TH2D("hSubEtaPhi","Sub Jet #eta vs. #phi;#phi;#eta", 126,0,6.3, 100,-1.0,1.0);
   TH3D *hPt_UE_BBCE = new TH3D("hPt_UE_BBCE","UE vs. BBC East Rate;Lead Jet p_{T} (GeV);Underlying Event (GeV);BBC East Rate", 300,0,300, 40,0,200, 3500,0,3500000);
   TH2D *hTowersVsRho = new TH2D("hTowersVsRho","# of Towers vs. UE;#rho (GeV);# of Towers", 80,0,35, 100,0,1000);
   TH2D *hLeadPtVsRho = new TH2D("hLeadPtVsRho","Lead Jet p_{T} vs UE;#rho (GeV);p_{T}^{lead} (GeV)", 70,0.05,35, 70,0,70);
 
-  TH2D *hscale = new TH2D( "hscale", "Underlying Event by Lead Jet p_{T};#rho (GeV);", 1,0,1, 60,0,30);
+  TH2D *hscale = new TH2D( "hscale", "Underlying Event by Lead Jet p_{T};#rho (GeV);", 60,0,30, 1,0,1 );
   
   TTree *jt = (TTree*) inFile->Get("HTjetTree");
   
@@ -60,7 +61,8 @@ int main() {
   for (int i=0; i<nEntries; ++i){
   // for (int i=0; i<1000; ++i){
     jt->GetEntry(i);
-    
+
+    hPrimaryVsGlobal->Fill(nPrimary,nGlobal);
     hPrimaryVsBBCE->Fill(nPrimary,BbcEastRate);
     hGlobalVsBBCE->Fill(nGlobal,BbcEastRate);
     hLeadEtaPhi->Fill(leadPhi,leadEta);
@@ -86,13 +88,13 @@ int main() {
   
   int ptBinLo[nPtBins] = { 0, 5, 10, 15, 25, 35, 50 };
   int ptBinHi[nPtBins] = { 5, 10, 15, 25, 35, 50, 70 };
-  TString ptBinString[nPtBins] = { "<5 GeV", "5-10 GeV", "10-15 GeV", "15-25 GeV",  "25-35 GeV", "35-50 GeV", "<50 GeV" };
+  TString ptBinString[nPtBins] = { "<5 GeV", "5-10 GeV", "10-15 GeV", "15-25 GeV",  "25-35 GeV", "35-50 GeV", ">50 GeV" };
   TString ptBinName[nPtBins] = { "_5", "_5_10", "_10_15", "_15_25", "_25_35", "_35_50", "_50" };
   int color[nPtBins] = { 29, 33, 34, 23, 22, 21, 20 };
   int marker[nPtBins] = { 1, 633, 613, 596, 839, 414, 797 };
   TString name, title;
 
-  TCanvas * c1 = new TCanvas( "c1" , "" ,0 ,23 ,1280 ,700 );       gStyle->SetOptStat(0);   // CANVAS
+  TCanvas * c1 = new TCanvas( "c1" , "" ,0 ,23 ,1280 ,700 );              // CANVAS
   c1->SetLogy();
   hscale->Draw();
 
@@ -104,6 +106,7 @@ int main() {
     name = "LeadPtVsRho" + ptBinName[i];
     title = ptBinString[i];
     hRho[i] = hLeadPtVsRho->ProjectionX( name, ptBinLo[i], ptBinHi[i] );       // PROJECT
+    hRho[i]->SetStats(0);
     hRho[i]->Scale( 1./hRho[i]->Integral() );                     // NORMALIZE
     hRho[i]->SetLineColor( color[i] );
     hRho[i]->SetMarkerStyle( marker[i] );
@@ -125,13 +128,17 @@ int main() {
   c1->SetSelected(c1);
   c1->SaveAs("plots/RhoByLeadPt.pdf","PDF");
 
+  gStyle->SetOptStat(1);
   TCanvas * c2 = new TCanvas( "c2" , "" ,0 ,23 ,1280 ,700 );              // CANVAS
-  hTowersVsRho->Draw("COLZ");
-  c2->SaveAs("plots/TowersVsRho.pdf","PDF");
   hLeadEtaPhi->Draw("COLZ");
   c2->SaveAs("plotshLeadEtaPhi.pdf","PDF");  
   hSubEtaPhi->Draw("COLZ");
   c2->SaveAs("plots/SubEtaPhi.pdf","PDF");
+  hPrimaryVsGlobal->Draw("COLZ");
+  c2->SaveAs("plots/PrimaryVsGlobal.pdf","PDF");  
+  hTowersVsRho->SetLogz();
+  hTowersVsRho->Draw("COLZ");
+  c2->SaveAs("plots/TowersVsRho.pdf","PDF");
 
   outFile->Write();
   outFile->Close();
