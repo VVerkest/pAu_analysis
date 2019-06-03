99dcc9ae08a2e3999e1d6c96107840ee2c8b7360
VVerkest <vverkest@gmail.com>
2019-06-03 16:31:33 -0400

    update histograms; change dijet phi definition to be lead jet phi; include BbcAdcSumEast; impose lead jet min pt of 10 GeV

--- src/pAu_HT_jets.cxx
+++ src/pAu_HT_jets.cxx
@@ -31,7 +31,7 @@ int main ( int argc, const char** argv ) {
     vector<string> arguments( argv+1, argv+argc );
     inFile = "production_pAu200_2015/HT/pAu_2015_200_HT*.root";
     outFile = "out/pAuJets_HT.root";
-    nEvents = 1000;
+    nEvents = 10000;
   }
   else { cerr<< "incorrect number of command line arguments"; return -1; }
 
@@ -46,30 +46,27 @@ int main ( int argc, const char** argv ) {
   double dca, pt, pz, eta, phi, pmin1, pmax1, pmin2, pmax2;
     
   int RunID, EventID, nTowers, nPrimary, nGlobal, nVertices, refMult, gRefMult, nJets, leadNcons, subNcons;
-  double Vx, Vy, Vz, BbcCoincidenceRate, BbcEastRate, BbcWestRate, vpdVz,  leadPt, leadEta, leadPhi, leadEt, subPt, subEta, subPhi, subEt, rho, sigma;
+  double Vx, Vy, Vz, BbcCoincidenceRate, BbcEastRate, BbcWestRate, BbcAdcSumEast, vpdVz,  leadPt, leadEta, leadPhi, leadEt, subPt, subEta, subPhi, subEt, rho, sigma;
   int towID, nHitsPoss, nHitsFit, Charge, nCons;
   double DCA, towEt, towEta, towPhi, trEta, trPhi, trPz, trPt, jetPt, jetEta, jetPhi, jetEt;
   
   TH1::SetDefaultSumw2();  TH2::SetDefaultSumw2();  TH3::SetDefaultSumw2();
 
-  TH3D *hPrimaryTracks = new TH3D( "hPrimaryTracks", "Primary Tracks: p_{T}, #eta, and #phi;p_{T} (GeV);#eta;#phi", 20,0,10, 24,-1.2,1.2, 128,-pi,pi );
   TH3D *hVertex = new TH3D( "hVertex", "Event Vertex;v_{x};v_{y};v_{z}", 60,-0.3,0.3, 60,-0.3,0.3, 160,-40,40 );
   TH1D *hTowersPerEvent = new TH1D("hTowersPerEvent","Tower Frequency;# of Towers", 700,0,700 );
   TH2D *hTowersPerRun = new TH2D("hTowersPerRun","Tower Frequency (per run);Run no.;# of Towers", 40000,16124000,16164000, 140,0,700 );
   TH1D *hPrimaryPerEvent = new TH1D("hPrimaryPerEvent","Primary Track Multiplicity (per event);# of Primary", 200,0,200 );
   TH2D *hPrimaryPerRun = new TH2D("hPrimaryPerRun","Primary Track Multiplicity (per run);Run no.;# of Primary", 40000,16124000,16164000, 40,0,200 );
   TH2D *hnPrimaryVSnTowers = new TH2D("hnPrimaryVSnTowers","# of Primary Tracks vs. # of Towers;# Towers;#Primary Tracks", 140,0,700, 40,0,200);
-  TH2D *hDCAvsPt = new TH2D("hDCAvsPt","DCA vs. p_{T};p_{T} GeV;DCA (cm)", 100,0.0,50.0, 70,0,3.5);
   TH2D *hPrimaryVsBBC = new TH2D("hPrimaryVsBBC","# Primary Tracks vs. BBC Coincidence Rate;BBC Rate;# Primary Tracks", 3500,0,3500000, 40,0,200 );
   TH2D *hGlobalVsBBC = new TH2D("hGlobalVsBBC","# Global Tracks vs. BBC Coincidence Rate;BBC Rate;# Global Tracks", 3500,0,3500000, 150,0,3000 );
-  TH2D *hTowEt = new TH2D("hTowEt","Tower E_{T} by ID;Tower ID;E_{T} (GeV)", 4800,0,4800, 60,0,30.0);
-  TH3D *hTowEtEtaPhi = new TH3D("hTowEtEtaPhi","Tower E_{T} vs. #eta vs. #phi;Tower E_{T} (GeV);Tower #eta;Tower #phi", 60,0,30.0, 100,-1.0,1.0, 128,-pi,pi );
-  TH1D *hTowerMult = new TH1D("hTowerMult","Tower Multiplicity by ID;Tower ID", 2400,0,4800);
   TH2D *hPrimaryVsBBCE = new TH2D("hPrimaryVsBBCE","# Primary Tracks vs. BBC East Rate;BBC East Rate;# Primary Tracks", 50000,0,5000000, 150,0,150 );
   TH2D *hGlobalVsBBCE = new TH2D("hGlobalVsBBCE","# Global Tracks vs. BBC East Rate;BBC East Rate;# Global Tracks", 50000,0,5000000, 300,0,3000 );
+  TH2D *hGlobalVsBBCsumE = new TH2D("hGlobalVsBBCsumE","# Global Tracks vs. BBC ADC East Sum;BBC ADC East Sum;# Global Tracks", 50000,0,5000000, 300,0,3000 );
   TH2D *hLeadEtaPhi = new TH2D("hLeadEtaPhi","Lead Jet #eta vs. #phi;#phi;#eta", 180,0,6.3, 100,-1.0,1.0);
   TH2D *hSubEtaPhi = new TH2D("hSubEtaPhi","Sub Jet #eta vs. #phi;#phi;#eta", 180,0,6.3, 100,-1.0,1.0);
   TH3D *hPt_UE_BBCE = new TH3D("hPt_UE_BBCE","UE vs. BBC East Rate;Lead Jet p_{T} (GeV);Underlying Event (GeV);BBC East Rate", 300,0,300, 40,0,200, 3500,0,3500000);
+  TH3D *hPt_UE_BBCsumE = new TH3D("hPt_UE_BBCsumE","UE vs. BBC ADC East Sum;Lead Jet p_{T} (GeV);Underlying Event (GeV);BBC ADC East Sum", 300,0,300, 40,0,200, 3500,0,3500000);
   TH2D *hTowersVsRho = new TH2D("hTowersVsRho","# of Towers vs. UE;#rho (GeV);# of Towers", 80,0,35, 100,0,1000);
   TH2D *hLeadPtVsRho = new TH2D("hLeadPtVsRho","Lead Jet p_{T} vs UE;#rho (GeV);p_{T}^{lead} (GeV)", 80,0,35, 140,0,70);
   
@@ -98,18 +95,7 @@ int main ( int argc, const char** argv ) {
     
     Vz = header->GetPrimaryVertexZ();
     if ( abs(Vz) > vzCut ) { continue; }
-
-    eID = Reader.GetNOfCurrentEvent();          EventID = eID;
-    rID = header->GetRunId();                        RunID = rID;
-
-    int npt = header->GetNOfPrimaryTracks();      nPrimary = npt;
-    int ntow = header->GetNOfTowers();               nTowers = ntow;
-    Vx = header->GetPrimaryVertexX();                                           Vy = header->GetPrimaryVertexY();
-    nGlobal = header->GetNGlobalTracks();                                    nVertices = header->GetNumberOfVertices();
-    refMult = header->GetReferenceMultiplicity();                           gRefMult = header->GetGReferenceMultiplicity();
-    BbcCoincidenceRate = header->GetBbcCoincidenceRate();        vpdVz = header->GetVpdVz();
-    BbcEastRate = header->GetBbcEastRate();                                  BbcWestRate = header->GetBbcWestRate();
-
+      
         //   JET-FINDING
     GatherParticles( container, rawParticles);     //  GATHERS ALL PARTICLES WITH    pT>=2.0GeV    and    |eta|<1.0
     ClusterSequence jetCluster( rawParticles, jet_def );           //  CLUSTER ALL JETS
@@ -122,7 +108,7 @@ int main ( int argc, const char** argv ) {
     double phi1 = rawJets[0].phi();     double phi2 = rawJets[1].phi();
     double dphi = fabs( fabs( phi1 - phi2 ) - pi );
 
-    if ( dphi> R || rawJets[0].pt()<2.0 || rawJets[1].pt()<2.0 ) { continue; }
+    if ( dphi> R || rawJets[0].pt()<10.0 || rawJets[1].pt()<2.0 ) { continue; }
     else {                        //  CREATE DIJET PAIR  
       leadPt = rawJets[0].pt();      leadEta = rawJets[0].eta();      leadPhi = rawJets[0].phi();      leadEt = rawJets[0].Et();
       vector<PseudoJet> LeadCons= rawJets[0].constituents();      leadNcons = LeadCons.size();
@@ -130,10 +116,8 @@ int main ( int argc, const char** argv ) {
       vector<PseudoJet> SubCons= rawJets[1].constituents();      subNcons = SubCons.size();
     }
     
-    double tanPhi = tan(phi1) + tan(phi2);          tanPhi /= 2;
-    double djAxisPhi = atan( tanPhi );       // qpi = quarter of pi
-    pmin1 = djAxisPhi + qpi;           pmax1 = djAxisPhi + (3*qpi);
-    pmin2 = djAxisPhi - (3*qpi);      pmax2 = djAxisPhi - qpi;
+    pmin1 = phi1 + qpi;           pmax1 = phi1 + (3*qpi);
+    pmin2 = phi1 - (3*qpi);      pmax2 = phi1 - qpi;
     Selector bgPhiRange = SelectorPhiRange( pmin1, pmax1 ) || SelectorPhiRange( pmin2, pmax2 );
     Selector bgSelector = bgPhiRange && SelectorAbsEtaMax( 1.0 );
 
@@ -147,44 +131,26 @@ int main ( int argc, const char** argv ) {
 
     // cout << "rho = "<<rho<<"\t  sigma = "<<sigma<<endl<<endl;
 
+    
+    eID = Reader.GetNOfCurrentEvent();          EventID = eID;
+    rID = header->GetRunId();                        RunID = rID;
+
+    int npt = header->GetNOfPrimaryTracks();      nPrimary = npt;
+    int ntow = header->GetNOfTowers();               nTowers = ntow;
+    Vx = header->GetPrimaryVertexX();                                           Vy = header->GetPrimaryVertexY();
+    nGlobal = header->GetNGlobalTracks();                                    nVertices = header->GetNumberOfVertices();
+    refMult = header->GetReferenceMultiplicity();                           gRefMult = header->GetGReferenceMultiplicity();
+    BbcCoincidenceRate = header->GetBbcCoincidenceRate();        vpdVz = header->GetVpdVz();
+    BbcEastRate = header->GetBbcEastRate();                                  BbcWestRate = header->GetBbcWestRate();
+    BbcAdcSumEast = header->GetBbcAdcSumEast();
 
     hLeadEtaPhi->Fill(leadPhi,leadEta);
     hSubEtaPhi->Fill(subPhi,subEta);
     hPt_UE_BBCE->Fill(leadPt,rho,BbcEastRate);
+    hPt_UE_BBCsumE->Fill(leadPt,rho,BbcAdcSumEast);
     hTowersVsRho->Fill(rho,nTowers);
     hLeadPtVsRho->Fill(rho,leadPt);
 
-    
-    for ( int i=0; i<npt; ++i ) {                                         //  FILL PRIMARY TRACK DATA
-
-      DCA = event->GetPrimaryTrack(i)->GetDCA();
-      if ( DCA > dcaCut ) { nPrimary -= 1;   continue; }                   // track DCA cut
-      trEta = (double) event->GetPrimaryTrack(i)->GetEta();
-      if ( abs(trEta) > etaCut ) { nPrimary -= 1;   continue; }            // track eta cut
-      trPt = (double) event->GetPrimaryTrack(i)->GetPt();
-      trPz = (double) event->GetPrimaryTrack(i)->GetPz();
-      trPhi = (double) event->GetPrimaryTrack(i)->GetPhi();
-
-      nHitsFit = event->GetPrimaryTrack(i)->GetNOfFittedHits();
-      nHitsPoss = event->GetPrimaryTrack(i)->GetNOfPossHits();
-
-      hPrimaryTracks->Fill( trPt, trEta, trPhi );
-      hDCAvsPt->Fill( trPt, DCA );
-    }
-
-    
-    for ( int i=0; i<ntow; ++i ) {                                         //  FILL TOWER DATA
-      towEt = event->GetTower(i)->GetEt();
-      eta = event->GetTower(i)->GetEta();
-      if ( abs(eta) > etaCut ) { nTowers -= 1;   continue; }            // tower eta cut
-      towEta = eta;
-      towPhi = event->GetTower(i)->GetPhi();
-      towID = event->GetTower(i)->GetId();
-
-      hTowEt->Fill( towID, towEt );
-      hTowEtEtaPhi->Fill( towEt, towEta, towPhi );
-      hTowerMult->Fill( towID );
-    }
 
     hVertex->Fill( Vx, Vy, Vz );                                        //  FILL HISTOGRAMS
     hTowersPerEvent->Fill( nTowers );
@@ -196,13 +162,12 @@ int main ( int argc, const char** argv ) {
     hGlobalVsBBC->Fill( BbcCoincidenceRate, nGlobal );
     hPrimaryVsBBCE->Fill(nPrimary,BbcEastRate);
     hGlobalVsBBCE->Fill(nGlobal,BbcEastRate);
-
+    hGlobalVsBBCsumE->(nGlobal,BbcAdcSumEast);
 
   }
   // ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~  END EVENT LOOP!  ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
 
-  hPrimaryTracks->Write();                //  WRITE HISTOGRAMS
-  hVertex->Write();
+  hVertex->Write();                             //  WRITE HISTOGRAMS
   hTowersPerEvent->Write();
   hTowersPerRun->Write();
   hPrimaryPerEvent->Write();
@@ -210,18 +175,16 @@ int main ( int argc, const char** argv ) {
   hnPrimaryVSnTowers->Write();
   hPrimaryVsBBC->Write();
   hGlobalVsBBC->Write();
-  hTowEt->Write();
-  hTowEtEtaPhi->Write();
-  hDCAvsPt->Write();
-  hTowerMult->Write();
   hPrimaryVsBBCE->Write();
   hGlobalVsBBCE->Write();
   hLeadEtaPhi->Write();
   hSubEtaPhi->Write();
   hPt_UE_BBCE->Write();
+  hPt_UE_BBCsumE->Write();
   hTowersVsRho->Write();
   hLeadPtVsRho->Write();
-
+  hGlobalVsBBCsumE->Write();
+  
   pAuFile->Close();
   
   return 0;
