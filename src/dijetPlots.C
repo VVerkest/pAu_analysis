7cd4d4c61b1ca3449a01e77c4a59c8aa2c13bd50
VVerkest <vverkest@gmail.com>
2019-06-10 18:38:51 -0400

    f

--- src/dijetPlots.C
+++ src/dijetPlots.C
@@ -96,7 +96,7 @@ void dijetPlots() {
   leg0->AddEntry((TObject*)0,"#bf{p_{T}^{Lead} (GeV)}", "");
   leg0->AddEntry((TObject*)0,"#bf{<#rho> (GeV)}", "");
   
-  hscale1->SetStats(0);  c0->cd();  c0->Clear();  /*hscale1->Draw();*/  c1->cd();  c0->Clear();  /*hscale1->Draw();*/
+  hscale1->SetStats(0);  c0->cd();  c0->Clear();  hscale1->Draw();  c1->cd();  c0->Clear();  hscale1->Draw();
   for ( int i=0; i<nPtBins; ++i ) {
     hUE_BBCsumE[i]->SetStats(0);    hUE_BBCsumE[i]->SetLineColor( color[i] );    hUE_BBCsumE[i]->SetMarkerStyle( marker[i] );    hUE_BBCsumE[i]->SetMarkerColor( color[i] );    
     hUE_BBCE[i]->SetStats(0);    hUE_BBCE[i]->SetLineColor( color[i] );    hUE_BBCE[i]->SetMarkerStyle( marker[i] );    hUE_BBCE[i]->SetMarkerColor( color[i] );    
@@ -109,8 +109,8 @@ void dijetPlots() {
   c0->cd();  hscale1->Draw();
   c1->cd();  hscale1->Draw();
   for ( int i=0; i<nPtBins; ++i ) {
-    c0->cd();         hUE_BBCE[i]->ProjectionY()->Draw("SAME");
-    c1->cd();         hUE_BBCsumE[i]->ProjectionY()->Draw("SAME");
+    c0->cd();         hUE_BBCE[i]->ProjectionX()->Draw("SAME");
+    c1->cd();         hUE_BBCsumE[i]->ProjectionX()->Draw("SAME");
   }
   c0->SaveAs( "plots/UE_BBCE_projection.pdf","PDF");
   c1->SaveAs( "plots/UE_BBCsumE_projection.pdf","PDF");
