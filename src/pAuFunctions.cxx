2534112b1e2a8ef3787d7dffaa008105a5b6defc
VVerkest <vverkest@gmail.com>
2019-06-16 15:42:22 -0400

    neutral

--- src/pAuFunctions.cxx
+++ src/pAuFunctions.cxx
@@ -20,7 +20,8 @@ namespace pAuAnalysis {
       fastjet::PseudoJet current = fastjet::PseudoJet( *sv );
       current.set_user_index( sv->GetCharge() );
 
-      if ( sv->GetCharge() == 0 )      { continue; }  // removes neutral particles
+      // if ( sv->GetCharge() == 0 )      { continue; }  // removes neutral particles
+      if ( sv->GetCharge() == 0 )      { continue; }  // removes charged particles
       if ( std::abs(current.eta()) > etaCut )      { continue; }  // removes particles with |eta|>1
       if ( current.pt() < partMinPt )      { continue; }  // removes particles with pt<0.2GeV
 
