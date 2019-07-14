7060a283077b9a66c89a6bed075a0c9162ec6e93
VVerkest <vverkest@gmail.com>
2019-07-14 18:33:02 -0400

    update

--- macros/HTJP2Plot.C
+++ macros/HTJP2Plot.C
@@ -5,85 +5,77 @@ void HTJP2Plot() {
 
   TH1::SetDefaultSumw2();  TH2::SetDefaultSumw2();  TH3::SetDefaultSumw2();
 
-  const int acn = 3;
-  TString BackgroundChargeBias[acn] = { "allBG", "chgBG", "neuBG" };
-  TString JetChargeBias[acn] = { "allJets", "chgJets", "neuJets" };
+  // const int acn = 3;
+  // TString BackgroundChargeBias[acn] = { "allBG", "chgBG", "neuBG" };
+  // TString JetChargeBias[acn] = { "allJets", "chgJets", "neuJets" };
   TString fileName;			TString name, title;			TFile* inFile[acn][acn];
+
+  TString BackgroundChargeBias = "allBG";     //  [  "allBG",  "chgBG",  "neuBG"  ]
+  TString JetChargeBias = "allJets";                  //  [ "allJets", "chgJets", "neuJets" ]
+
+  fileName = "out/HTJP2/pAu_2015_200_" + BackgroundChargeBias + "_" + JetChargeBias + ".root";
+  inFile = new TFile( fileName, "READ" );
+
+  TH1D *hTowersPerEvent = (TH1D*) inFile->Get("hTowersPerEvent");			hTowersPerEvent->Scale(1.0/hTowersPerEvent->Integral("WIDTH"));
+  TH1D *hPrimaryPerEvent = (TH1D*) inFile->Get("hPrimaryPerEvent");			hPrimaryPerEvent->Scale(1.0/hPrimaryPerEvent->Integral("WIDTH"));
+  TH2D *hChgVsNeuBG = (TH2D*) inFile->Get("hChgVsNeuBG");				hChgVsNeuBG->Scale(1.0/hChgVsNeuBG->Integral("WIDTH"));
+  TH2D *hTowersPerRun = (TH2D*) inFile->Get("hTowersPerRun");	       		hTowersPerRun->Scale(1.0/hTowersPerRun->Integral("WIDTH"));
+  TH2D *hPrimaryPerRun = (TH2D*) inFile->Get("hPrimaryPerRun");		       	hPrimaryPerRun->Scale(1.0/hPrimaryPerRun->Integral("WIDTH"));
+  TH2D *hnPrimaryVSnTowers = (TH2D*) inFile->Get("hnPrimaryVSnTowers");	hnPrimaryVSnTowers->Scale(1.0/hnPrimaryVSnTowers->Integral("WIDTH"));
+  TH2D *hPrimaryVsBBC = (TH2D*) inFile->Get("hPrimaryVsBBC");				hPrimaryVsBBC->Scale(1.0/hPrimaryVsBBC->Integral("WIDTH"));
+  TH2D *hPrimaryVsGlobal = (TH2D*) inFile->Get("hPrimaryVsGlobal");		       	hPrimaryVsGlobal->Scale(1.0/hPrimaryVsGlobal->Integral("WIDTH"));
+  // TH2D *hGlobalVsBBC = (TH2D*) inFile->Get("hGlobalVsBBC");				hGlobalVsBBC->Scale(1.0/hGlobalVsBBC->Integral("WIDTH"));
+  // TH2D *hPrimaryVsBBCE = (TH2D*) inFile->Get("hPrimaryVsBBCE");			hPrimaryVsBBCE->Scale(1.0/hPrimaryVsBBCE->Integral("WIDTH"));
+  // TH2D *hGlobalVsBBCE = (TH2D*) inFile->Get("hGlobalVsBBCE");				hGlobalVsBBCE->Scale(1.0/hGlobalVsBBCE->Integral("WIDTH"));
+  // TH2D *hPrimaryVsBBCsumE = (TH2D*) inFile->Get("hPrimaryVsBBCsumE");		hPrimaryVsBBCsumE->Scale(1.0/hPrimaryVsBBCsumE->Integral("WIDTH"));
+  // TH2D *hTowersVsBBCsumE = (TH2D*) inFile->Get("hTowersVsBBCsumE");		hTowersVsBBCsumE->Scale(1.0/hTowersVsBBCsumE->Integral("WIDTH"));
+  TH2D *hPrimaryVsRho = (TH2D*) inFile->Get("hPrimaryVsRho");				hPrimaryVsRho->Scale(1.0/hPrimaryVsRho->Integral("WIDTH"));
+  TH2D *hGlobalVsRho = (TH2D*) inFile->Get("hGlobalVsRho");				hGlobalVsRho->Scale(1.0/hGlobalVsRho->Integral("WIDTH"));
+  TH2D *hSubEtaPhi = (TH2D*) inFile->Get("hSubEtaPhi");					hSubEtaPhi->Scale(1.0/hSubEtaPhi->Integral("WIDTH"));
+  TH2D *hTowersVsRho = (TH2D*) inFile->Get("hTowersVsRho");				hTowersVsRho->Scale(1.0/hTowersVsRho->Integral("WIDTH"));
+  TH2D *hLeadPtVsRho = (TH2D*) inFile->Get("hLeadPtVsRho");
+  TH3D *hPt_UE_BBCE = (TH3D*) inFile->Get(hPt_UE_BBCE"");
+  TH3D *hPt_UE_BBCsumE = (TH3D*) inFile->Get("hPt_UE_BBCsumE");
+  TH3D *hBG = (TH3D*) inFile->Get("hBG");
+  TH3D *hNEUTRAL = (TH3D*) inFile->Get("hBG");
+  TH3D *hPt_UE_RefMult = (TH3D*) inFile->Get("hPt_UE_RefMult");
+  TH3D *hCHARGED = (TH3D*) inFile->Get("hPt_UE_RefMult");
+  TH3D *hAllJetsPtRhoEta = (TH3D*) inFile->Get("hAllJetsPtRhoEta");
+  TH3D *hPartPtEtaPhi = (TH3D*) inFile->Get("hPartPtEtaPhi");
+  TH3D *hAllPtEtaPhi = (TH3D*) inFile->Get("hAllPtEtaPhi");
+  TH3D *hPartPtDEtaDPhi = (TH3D*) inFile->Get("hPartPtDEtaDPhi");
+  TH3D *hVertex = (TH3D*) inFile->Get("hVertex");
+  TH3D *hLeadPtEtaPhi = (TH3D*) inFile->Get("hLeadPtEtaPhi");
+  TH3D *hAllJetsPtEtaPhi = (TH3D*) inFile->Get("hAllJetsPtEtaPhi");
+
+  TH
+
+  inFile->Close();
+
+  TCanvas * c0 = new TCanvas( "c0" , "" ,700 ,500 );              // CANVAS
   
-  TH1D *hTowersPerEvent[bb][jb];
-  TH1D *hPrimaryPerEvent[bb][jb];
-  TH2D *hChgVsNeuBG[bb][jb];
-  TH2D *hTowersPerRun[bb][jb];
-  TH2D *hPrimaryPerRun[bb][jb];
-  TH2D *hnPrimaryVSnTowers[bb][jb];
-  TH2D *hPrimaryVsBBC[bb][jb];
-  TH2D *hPrimaryVsGlobal[bb][jb];
-  TH2D *hGlobalVsBBC[bb][jb];
-  TH2D *hPrimaryVsBBCE[bb][jb];
-  TH2D *hGlobalVsBBCE[bb][jb];
-  TH2D *hPrimaryVsBBCsumE[bb][jb];
-  TH2D *hTowersVsBBCsumE[bb][jb];
-  TH2D *hPrimaryVsRho[bb][jb];
-  TH2D *hGlobalVsRho[bb][jb];
-  TH2D *hSubEtaPhi[bb][jb];
-  TH2D *hTowersVsRho[bb][jb];
-  TH2D *hLeadPtVsRho[bb][jb];
-  TH3D *hPt_UE_BBCE[bb][jb];
-  TH3D *hPt_UE_BBCsumE[bb][jb];
-  TH3D *hBG[bb][jb];
-  TH3D *hNEUTRAL[bb][jb];
-  TH3D *hPt_UE_RefMult[bb][jb];
-  TH3D *hCHARGED[bb][jb];
-  TH3D *hAllJetsPtRhoEta[bb][jb];
-  TH3D *hPartPtEtaPhi[bb][jb];
-  TH3D *hAllPtEtaPhi[bb][jb];
-  TH3D *hPartPtDEtaDPhi[bb][jb];
-  TH3D *hVertex[bb][jb];
-  TH3D *hLeadPtEtaPhi[bb][jb];
-  TH3D *hAllJetsPtEtaPhi[bb][jb];
+  c0->SetLogy();
+  
+  hTowersPerEvent->Draw();				name = "plots/HTJP2/TowersPerEvent_" + BackgroundChargeBias + "_" + JetChargeBias + ".pdf";			c0->SaveAs( name, "PDF" );
 
-  for ( int bb=0; bb<acn; ++bb ) {
-    for ( int jb=0; jb<acn; ++jb ) {
+  hPrimaryPerEvent->Draw();				name = "plots/HTJP2/PrimaryPerEvent_" + BackgroundChargeBias + "_" + JetChargeBias + ".pdf";			c0->SaveAs( name, "PDF" );
 
-      if ( bb!=jb && bb!=0 && jb!=0 ) { continue; }  // skip (chgBG,neuJet) and (neuBG,chgJet)
-      
-      fileName = "out/HTJP2/pAu_2015_200_" + BackgroundChargeBias + "_" + JetChargeBias + ".root";
-      inFile[bb][jb] = new TFile( fileName, "READ" );
+  TH1D *hAllJetsPt = (TH1D*) hAllJetsPtEtaPhi->ProjectionX();				hAllJetsPt->Scale(1.0/hAllJetsPt->Integral());
+  hAllJetsPt->Draw();					name = "plots/HTJP2/AllJetsPt_" + BackgroundChargeBias + "_" + JetChargeBias + ".pdf";					c0->SaveAs( name, "PDF" );
 
-      hTowersPerEvent[bb][jb] = (TH1D*) inFile[bb][jb]->Get("hTowersPerEvent");
-      hPrimaryPerEvent[bb][jb] = (TH1D*) inFile[bb][jb]->Get("hPrimaryPerEvent");
-      hChgVsNeuBG[bb][jb] = (TH2D*) inFile[bb][jb]->Get("hChgVsNeuBG");
-      hTowersPerRun[bb][jb] = (TH2D*) inFile[bb][jb]->Get("hTowersPerRun");
-      hPrimaryPerRun[bb][jb] = (TH2D*) inFile[bb][jb]->Get("hPrimaryPerRun");
-      hnPrimaryVSnTowers[bb][jb] = (TH2D*) inFile[bb][jb]->Get("hnPrimaryVSnTowers");
-      hPrimaryVsBBC[bb][jb] = (TH2D*) inFile[bb][jb]->Get("hPrimaryVsBBC");
-      hPrimaryVsGlobal[bb][jb] = (TH2D*) inFile[bb][jb]->Get("hPrimaryVsGlobal");
-      hGlobalVsBBC[bb][jb] = (TH2D*) inFile[bb][jb]->Get("hGlobalVsBBC");
-      hPrimaryVsBBCE[bb][jb] = (TH2D*) inFile[bb][jb]->Get("hPrimaryVsBBCE");
-      hGlobalVsBBCE[bb][jb] = (TH2D*) inFile[bb][jb]->Get("hGlobalVsBBCE");
-      hPrimaryVsBBCsumE[bb][jb] = (TH2D*) inFile[bb][jb]->Get("hPrimaryVsBBCsumE");
-      hTowersVsBBCsumE[bb][jb] = (TH2D*) inFile[bb][jb]->Get("hTowersVsBBCsumE");
-      hPrimaryVsRho[bb][jb] = (TH2D*) inFile[bb][jb]->Get("hPrimaryVsRho");
-      hGlobalVsRho[bb][jb] = (TH2D*) inFile[bb][jb]->Get("hGlobalVsRho");
-      hSubEtaPhi[bb][jb] = (TH2D*) inFile[bb][jb]->Get("hSubEtaPhi");
-      hTowersVsRho[bb][jb] = (TH2D*) inFile[bb][jb]->Get("hTowersVsRho");
-      hLeadPtVsRho[bb][jb] = (TH2D*) inFile[bb][jb]->Get("hLeadPtVsRho");
-      hPt_UE_BBCE[bb][jb] = (TH3D*) inFile[bb][jb]->Get(hPt_UE_BBCE"");
-      hPt_UE_BBCsumE[bb][jb] = (TH3D*) inFile[bb][jb]->Get("hPt_UE_BBCsumE");
-      hBG[bb][jb] = (TH3D*) inFile[bb][jb]->Get("hBG");
-      hNEUTRAL[bb][jb] = (TH3D*) inFile[bb][jb]->Get("hBG");
-      hPt_UE_RefMult[bb][jb] = (TH3D*) inFile[bb][jb]->Get("hPt_UE_RefMult");
-      hCHARGED[bb][jb] = (TH3D*) inFile[bb][jb]->Get("hPt_UE_RefMult");
-      hAllJetsPtRhoEta[bb][jb] = (TH3D*) inFile[bb][jb]->Get("hAllJetsPtRhoEta");
-      hPartPtEtaPhi[bb][jb] = (TH3D*) inFile[bb][jb]->Get("hPartPtEtaPhi");
-      hAllPtEtaPhi[bb][jb] = (TH3D*) inFile[bb][jb]->Get("hAllPtEtaPhi");
-      hPartPtDEtaDPhi[bb][jb] = (TH3D*) inFile[bb][jb]->Get("hPartPtDEtaDPhi");
-      hVertex[bb][jb] = (TH3D*) inFile[bb][jb]->Get("hVertex");
-      hLeadPtEtaPhi[bb][jb] = (TH3D*) inFile[bb][jb]->Get("hLeadPtEtaPhi");
-      hAllJetsPtEtaPhi[bb][jb] = (TH3D*) inFile[bb][jb]->Get("hAllJetsPtEtaPhi");
-    }
-  }
+  c0->SetLogy(0);		c0->SetLogz();
 
+  hChgVsNeuBG->Draw("COLZ");			name = "plots/HTJP2/ChgVsNeuBG_" + BackgroundChargeBias + "_" + JetChargeBias + ".pdf";				c0->SaveAs( name, "PDF" );
+
+  hnPrimaryVSnTowers->Draw("COLZ");		name = "plots/HTJP2/nPrimaryVSnTowers_" + BackgroundChargeBias + "_" + JetChargeBias + ".pdf";			c0->SaveAs( name, "PDF" );
+
+  hPrimaryVsBBC->Draw("COLZ");			name = "plots/HTJP2/PrimaryVsBBC_" + BackgroundChargeBias + "_" + JetChargeBias + ".pdf";				c0->SaveAs( name, "PDF" );
+
+  hPrimaryVsGlobal->Draw("COLZ");		name = "plots/HTJP2/PrimaryVsGlobal_" + BackgroundChargeBias + "_" + JetChargeBias + ".pdf";			c0->SaveAs( name, "PDF" );
+
+  hPrimaryVsRho->Draw("COLZ");			name = "plots/HTJP2/PrimaryVsRho_" + BackgroundChargeBias + "_" + JetChargeBias + ".pdf";				c0->SaveAs( name, "PDF" );
+
+  hGlobalVsRho->Draw("COLZ");			name = "plots/HTJP2/GlobalVsRho_" + BackgroundChargeBias + "_" + JetChargeBias + ".pdf";				c0->SaveAs( name, "PDF" );
 
 
 }
