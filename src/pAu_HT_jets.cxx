adaa6956e94ef86e2fb43db233f895e8124fe38a
VVerkest <vverkest@gmail.com>
2019-07-20 19:56:03 -0400

    define variables

--- src/pAu_HT_jets.cxx
+++ src/pAu_HT_jets.cxx
@@ -17,7 +17,7 @@ int main ( int argc, const char** argv ) {         // funcions and cuts specifie
   
   vector<string> arguments( argv+1, argv+argc );
   if ( argc ==  4 ) {    inFile = arguments[0];    outFile = arguments[1];    nEvents = atoi(arguments[2].c_str()); }
-  else if ( argc==1 ) { inFile="production_pAu200_2015/HT/pAu_2015_200_HT*.root"; outFile="out/pAuJets_HTJP2.root"; nEvents=1000000; }
+  else if ( argc==1 ) { inFile="production_pAu200_2015/HT/pAu_2015_200_HT*.root"; outFile="out/pAuJets_HTJP2.root"; nEvents=100000; }
   else { cerr<< "incorrect number of command line arguments"; return -1; }
 
   TH1::SetDefaultSumw2();  TH2::SetDefaultSumw2();  TH3::SetDefaultSumw2();
@@ -45,7 +45,7 @@ int main ( int argc, const char** argv ) {         // funcions and cuts specifie
   TStarJetPicoReader Reader;                                int numEvents = nEvents;        // total events in HT: 152,007,032
   InitReader( Reader, Chain, numEvents );
 
-  double leadPt, leadEta, leadPhi, eastRho, midRho, westRho;
+  double Vz, leadPt, leadEta, leadPhi, eastRho, midRho, westRho;		PseudoJet leadJet;
 
 
   // ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~  BEGIN EVENT LOOP!  ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
@@ -76,26 +76,26 @@ int main ( int argc, const char** argv ) {         // funcions and cuts specifie
 	
 	vector<PseudoJet> rawJets = sorted_by_pt( etaPtSelector( jetCluster.inclusive_jets() ) );     // EXTRACT SELECTED JETS
 	
-	if ( rawJets.size()>0 ) { PseudoJet leadJet = rawJets[0]; }
+	if ( rawJets.size()>0 ) { leadJet = rawJets[0]; }
 	else { continue; }
 	
 	for ( int c=0; c<3; ++c ) {	  //  BACKGROUND ESTIMATION 
 	  chgParticles.clear();		neuParticles.clear();
 
-	  if ( BackgroundChargeBias[c]=="allBG" || BackgroundChargeBias[c]=="chgBG" ) { GatherChargedBG( leadJet, container, chgParticles); }      //  Gather background particles 
+	  if ( BackgroundChargeBias[c]=="allBG" || BackgroundChargeBias[c]=="chgBG" ) { GatherChargedBG( leadJet, container, chgParticlesicles); }      //  Gather background particles 
 	  if ( BackgroundChargeBias[c]=="allBG" || BackgroundChargeBias[c]=="neuBG" ) { GatherNeutralBG( leadJet, container, neuParticles); }
 
 	  double eastSum = 0;	  double midSum = 0;	  double westSum = 0;	  
-	  for (int i=0; i<chgPart.size(); ++i) {
-	    if ( etaLo[0] < chgPart[i] < etaHi[0]  ) { eastSum+=chgPart[i].pt(); }
-	    else if ( etaLo[1] < chgPart[i] < etaHi[1]  ) { midSum+=chgPart[i].pt(); }
-	    else if ( etaLo[2] < chgPart[i] < etaHi[2]  ) { westSum+=chgPart[i].pt(); }
+	  for (int i=0; i<chgParticles.size(); ++i) {
+	    if ( etaLo[0] < chgParticles[i] < etaHi[0]  ) { eastSum+=chgParticles[i].pt(); }
+	    else if ( etaLo[1] < chgParticles[i] < etaHi[1]  ) { midSum+=chgParticles[i].pt(); }
+	    else if ( etaLo[2] < chgParticles[i] < etaHi[2]  ) { westSum+=chgParticles[i].pt(); }
 	    else { cerr<<"particle |eta|>1"<<endl;        continue; }
 	  }
-	  for (int i=0; i<neuPart.size(); ++i) {
-	    if ( etaLo[0] < neuPart[i] < etaHi[0]  ) { eastSum+=neuPart[i].pt(); }
-	    else if ( etaLo[1] < neuPart[i] < etaHi[1]  ) { midSum+=neuPart[i].pt(); }
-	    else if ( etaLo[2] < neuPart[i] < etaHi[2]  ) { westSum+=neuPart[i].pt(); }
+	  for (int i=0; i<neuParticles.size(); ++i) {
+	    if ( etaLo[0] < neuParticles[i] < etaHi[0]  ) { eastSum+=neuParticles[i].pt(); }
+	    else if ( etaLo[1] < neuParticles[i] < etaHi[1]  ) { midSum+=neuParticles[i].pt(); }
+	    else if ( etaLo[2] < neuParticles[i] < etaHi[2]  ) { westSum+=neuParticles[i].pt(); }
 	    else { cerr<<"particle |eta|>1"<<endl;        continue; }
 	  }
 
