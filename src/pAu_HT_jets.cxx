daead6ed4f3891e865ef9c020793ac5a37dd378a
VVerkest <vverkest@gmail.com>
2019-07-21 15:12:45 -0400

    cout

--- src/pAu_HT_jets.cxx
+++ src/pAu_HT_jets.cxx
@@ -47,11 +47,7 @@ int main ( int argc, const char** argv ) {         // funcions and cuts specifie
   InitReader( Reader, Chain, numEvents );
 
   double Vz, leadPt, leadEta, leadPhi, eastRho, midRho, westRho;		PseudoJet leadJet;
-
-  cout<<etaLo[0]<<"-"<<etaHi[0]<<endl;
-  cout<<etaLo[1]<<"-"<<etaHi[1]<<endl;
-  cout<<etaLo[2]<<"-"<<etaHi[2]<<endl;
-
+  int blub = 0;
   // ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~  BEGIN EVENT LOOP!  ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
   while ( Reader.NextEvent() ) {
 
@@ -97,7 +93,10 @@ int main ( int argc, const char** argv ) {         // funcions and cuts specifie
 
 	    hBG->Fill( BGparticles[i].pt(), BGparticles[i].phi(), BGparticles[i].eta() );
 
-	    if ( etaLo[0] <= BGparticles[i].eta() <= etaHi[0]  ) { eastSum+=BGparticles[i].pt(); }
+	    if ( etaLo[0] <= BGparticles[i].eta() <= etaHi[0]  ) {
+	      eastSum+=BGparticles[i].pt();
+	      blub+=1; cout<<blub<<endl;
+	    }
 	    else if ( etaLo[1] < BGparticles[i].eta() < etaHi[1]  ) { midSum+=BGparticles[i].pt(); }
 	    else if ( etaLo[2] <= BGparticles[i].eta() <= etaHi[2]  ) { westSum+=BGparticles[i].pt(); }
 	    else { cerr<<BGparticles[i].eta()<<endl;        continue; }
