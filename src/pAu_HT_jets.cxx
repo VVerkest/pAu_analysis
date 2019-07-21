1e6a407d8763ade22a5872c207f00c9b1e35c016
VVerkest <vverkest@gmail.com>
2019-07-20 21:38:24 -0400

    debug

--- src/pAu_HT_jets.cxx
+++ src/pAu_HT_jets.cxx
@@ -101,7 +101,9 @@ int main ( int argc, const char** argv ) {         // funcions and cuts specifie
 	  }
 
 	  eastRho = eastSum/eastArea;			midRho = midSum/midArea;			westRho = westSum/westArea;
-	  
+	  if ( eastRho == 0.0 ) { cout<<"caw caw\t"; }
+	  if ( midRho == 0.0 ) { cout<<"woot woot\t"; }
+	  if ( westRho == 0.0 ) { cout<<"braafffffppptt\t"; }
 	  hRhoByEta[p][e][c]->AddBinContent( 1, eastRho );
 	  hRhoByEta[p][e][c]->AddBinContent( 3, midRho );
 	  hRhoByEta[p][e][c]->AddBinContent( 5, westRho );
