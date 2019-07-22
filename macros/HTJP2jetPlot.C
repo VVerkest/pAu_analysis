16f2f4d3679177d2d37d2e3d39234dc0db2a91e0
VVerkest <vverkest@gmail.com>
2019-07-22 14:47:53 -0400

    scale by entries

--- macros/HTJP2jetPlot.C
+++ macros/HTJP2jetPlot.C
@@ -48,7 +48,7 @@ void HTJP2jetPlot(){
       for ( int c=0; c<3; ++c ) {
 
 	TString name = "pRho" + ptBinName[p] + etaBinName[e] + BackgroundChargeBias[c];
-	// hRhoByEta[p][e][c]->Scale(1./hRhoByEta[p][e][c]->Integral());
+	hRhoByEta[p][e][c]->Scale(1./hRhoByEta[p][e][c]->Entries());
 	hRhoByEta[p][e][c]->SetMarkerSize(3);
 	hRhoByEta[p][e][c]->ProfileX(name)->Draw("SAME");
 
