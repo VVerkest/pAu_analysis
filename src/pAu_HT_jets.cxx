6453b30cc72fbdbcf5b0d5976d8f7d1402589ac4
VVerkest <vverkest@gmail.com>
2019-07-22 15:59:47 -0400

    define

--- src/pAu_HT_jets.cxx
+++ src/pAu_HT_jets.cxx
@@ -36,7 +36,7 @@ int main ( int argc, const char** argv ) {         // funcions and cuts specifie
 
 	name = "hBG" + ptBinName[p] + etaBinName[e] + BackgroundChargeBias[c];
 	title = ptBinString[p] + " " + etaBinString[e] + " " + BackgroundChargeBias[c] +" Background Particles;Background Particle p_{T}(GeV);Particle #phi;Particle #eta";
-	hBG = new TH3D(name, title, 80,0,20, 120,0,2*pi, 40,-1,1 );
+	hBG[p][e][c] = new TH3D(name, title, 80,0,20, 120,0,2*pi, 40,-1,1 );
       }
     }
   }
