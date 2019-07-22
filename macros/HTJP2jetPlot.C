15e73437d190690f4fcb99f25fab4fb27637085f
VVerkest <vverkest@gmail.com>
2019-07-22 17:31:52 -0400

    align title

--- macros/HTJP2jetPlot.C
+++ macros/HTJP2jetPlot.C
@@ -44,6 +44,7 @@ void HTJP2jetPlot(){
   cTitle->AddText("UnderlyingEvent");
   cTitle->SetFillStyle(0);
   cTitle->SetLineWidth(0);
+  cTitle->SetTextAlign(23);
   cTitle->Draw();
     
   c0->Divide(nEtaBins,nPtBins,0,0);
