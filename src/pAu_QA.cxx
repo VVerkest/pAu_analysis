f02b2c541c635bbd29f87afc000d490e984015e7
VVerkest <vverkest@gmail.com>
2019-09-04 13:15:11 -0400

    Update pAu_QA.cxx

--- src/pAu_QA.cxx
+++ src/pAu_QA.cxx
@@ -76,7 +76,7 @@ int main ( int argc, const char** argv ) {         // funcions and cuts specifie
   TChain* Chain = new TChain( "JetTree" );          Chain->Add( inFile.c_str() );
   TStarJetPicoReader Reader;                                int numEvents = number_of_events;        // total events in HT: 152,007,032
   InitReader( Reader, Chain, bad_tower_option, numEvents );
-  TStarJetPicoTowerCuts* towCuts = reader.GetTowerCuts();
+  TStarJetPicoTowerCuts* towCuts = Reader.GetTowerCuts();
 
   // ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~  BEGIN EVENT LOOP!  ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
   while ( Reader.NextEvent() ) {
@@ -125,7 +125,7 @@ int main ( int argc, const char** argv ) {         // funcions and cuts specifie
       trig = (TStarJetPicoTriggerInfo *)event->GetTrigObj(i);
       if ( trig->isBHT2() ) {
 	double trigTowId = trig->GetId();
-	if ( !towCuts->IsTowerOK( trigTowId ) ) { continue; }
+	if ( !TStarJetPicoTowerCuts::towCuts->IsTowerOK( trigTowId ) ) { continue; }
 	else {
 	hTriggerTowerId->Fill( trigTowId );
 	hTriggerEtaPhi->Fill( trig->GetEta(), trig->GetPhi() );
