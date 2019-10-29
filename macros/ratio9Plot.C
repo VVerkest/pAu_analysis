33a5afd7b846b6c2a31ac91e8de17b07bfaa2789
VVerkest <vverkest@gmail.com>
2019-10-29 00:29:48 -0400

    FINAL!

--- macros/ratio9Plot.C
+++ macros/ratio9Plot.C
@@ -28,7 +28,7 @@ void ratio9Plot(){
   
   gStyle->SetOptStat(0);
   TH1D *pRhoRatio[nPtBins][nEtaBins][nChgBins];
-  TH2D *sRhoRatio = new TH2D("sRhoRatio","", 3,-1.5,1.5, 10,0.855,1.05);
+  TH2D *sRhoRatio = new TH2D("sRhoRatio","", 3,-1.5,1.5, 10,0.855,1.0);
   sRhoRatio->GetXaxis()->SetLabelSize(0);
   sRhoRatio->GetYaxis()->SetLabelSize(0.06);
   sRhoRatio->GetYaxis()->SetNdivisions(12);
