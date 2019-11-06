c651580b98dfb0b470c51ac5b41ffff64e16796e
VVerkest <vverkest@gmail.com>
2019-11-05 19:32:35 -0500

    HiEA dijet

--- src/pAu_HT_dijets.cxx
+++ src/pAu_HT_dijets.cxx
@@ -18,10 +18,10 @@ int main ( int argc, const char** argv ) {         // funcions and cuts specifie
   else if ( argc==1 ) { inFile="production_pAu200_2015/HT/pAu_2015_200_HT*.root"; outFile="out/HT/pAuJets.root"; number_of_events=-1; }
   else { cerr<< "incorrect number of command line arguments"; return -1; }
 
-  bool dijet = true;
+  bool dijet = false;
   // Sort by Event Activity per Dave's definitions (https://drupal.star.bnl.gov/STAR/system/files/QM2019_Stewart_10.pdf)
   double BBCEmin = 0;        double BBCEmax = 100000;
-  bool HiEA = false;		if ( HiEA==true ){ BBCEmin = 38000;    BBCEmax = 100000; }
+  bool HiEA = true;		if ( HiEA==true ){ BBCEmin = 38000;    BBCEmax = 100000; }
   bool MidEA = false;	if ( MidEA==true ){ BBCEmin = 16000;    BBCEmax = 38000; }
   bool LoEA = false;		if ( LoEA==true ){ BBCEmin = 8000;    BBCEmax = 16000; }
   
