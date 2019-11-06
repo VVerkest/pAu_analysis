20c640700a0177d8b5c6fd6adfd14805c8ad6a15
VVerkest <vverkest@gmail.com>
2019-11-06 14:16:46 -0500

    Update EAratioPlots.C

--- macros/EAratioPlots.C
+++ macros/EAratioPlots.C
@@ -55,11 +55,11 @@ void EAratioPlots(){
 
   
   for (int ea=0; ea<EA; ++ea) {
-    for (int dj=0; dj<1; ++dj) {
+    for (int dj=0; dj<DJ; ++dj) {
       fileName[ea][dj] = "out/HTdijets/" + activity[ea] + "/pAu_2015_HT" + jetdijet[dj] + ".root";
       cout << fileName[ea][dj] <<endl;
       File[ea][dj] = new TFile( fileName[ea][dj], "READ" );
-
+      cout << File[ea][dj] << endl;
       for ( int p=0; p<3; ++p ) {
 	for ( int e=0; e<3; ++e ) {
 	
