10df09636f267e91c0c9ebb4835c321b66a24b86
VVerkest <vverkest@gmail.com>
2019-11-08 12:40:42 -0500

    Update HTjetPlot.C

--- macros/HTjetPlot.C
+++ macros/HTjetPlot.C
@@ -9,10 +9,6 @@ void HTjetPlot() {
   // TString BackgroundChargeBias[acn] = { "allBG", "chgBG", "neuBG" };
   // TString JetChargeBias[acn] = { "allJets", "chgJets", "neuJets" };
   TString fileName, name, title, Ndj, avg, sigma, jdj, ea;			double scale;
-
-  // TString BackgroundChargeBias = "allBG";		TString BackgroundChargeString = "All Background";			//  [  "allBG",  "chgBG",  "neuBG"  ]
-  // TString BackgroundChargeBias = "chgBG";		TString BackgroundChargeString = "Charged Background";
-  // TString BackgroundChargeBias = "neuBG";		TString BackgroundChargeString = "Neutral Background";
   
   jdj = "jets";
   // jdj = "dijets";
@@ -99,6 +95,17 @@ void HTjetPlot() {
   int color[nPtBins] = { 879, 856, 896 };
   int marker[nPtBins] = { 33, 21, 20 };
   
+  // TCanvas * cBG0 = new TCanvas( "cBG0" , "" ,0 ,23 ,1280 ,700 );
+  // cBG0->SetTopMargin(0.4);
+  // TPaveText *cTitle = new TPaveText(0.345843,.881306,0.655712,.980712,"NB");
+  // cTitle->AddText( ea + jdj + ": Charged Background" );
+  // cTitle->SetFillStyle(0);
+  // cTitle->SetLineWidth(0);
+  // cTitle->SetTextAlign(21);
+  // cTitle->Draw();
+    
+  // cBG0->Divide(nEtaBins,2,0,0);
+
   TH1D *hRho[nPtBins];		TH1D *hLeadEta[nPtBins];		TH1D *hSubEta[nPtBins];
 
   TCanvas * c0 = new TCanvas( "c0" , "" ,700 ,500 );              // CANVAS
@@ -137,46 +144,11 @@ void HTjetPlot() {
   c0->SaveAs( name , "PDF");
   
   hLeadPtEtaPhi->GetXaxis()->SetRangeUser(0.0,100.0);
-
-
-  // name = JetChargeString + ": Sub Jet #eta by p_{T};#eta;";
-  // TH2D *sSubEta = new TH2D( "sSubEta", name, 40,-1.0,1.0, 10,0.0, 2.0 );
-  // sSubEta->SetStats(0);
-  
-  // TLegend *leg1 = new TLegend(0.65, 0.65, 0.9, 0.9,NULL,"brNDC");    // LEGEND
-  // leg1->SetBorderSize(1);   leg1->SetLineColor(1);   leg1->SetLineStyle(1);   leg1->SetLineWidth(1);   leg1->SetFillColor(0);   leg1->SetFillStyle(1001);
-  // leg1->SetNColumns(3);
-  // leg1->AddEntry((TObject*)0,"#bf{p_{T}^{Sub} (GeV)}", "");
-  // leg1->AddEntry((TObject*)0,"#bf{# of Dijets}", "");
-  // leg1->AddEntry((TObject*)0,"#bf{<#eta>}", "");
-
-  // sSubEta->Draw();
-  // for ( int i=0; i<nPtBins; ++i ) {
-  //   name = "SubEta" + SubPtBinName[i];      title = SubPtBinString[i];
-  //   hSubPtEtaPhi->GetXaxis()->SetRangeUser(SubPtBinLo[i], SubPtBinHi[i]);
-  //   hSubEta[i] = (TH1D*) hSubPtEtaPhi->Project3D( "Y" );       // PROJECT
-  //   hSubEta[i]->SetNameTitle(name,title);
-  //   hSubEta[i]->SetStats(0);
-  //   hSubEta[i]->Scale( 1./hSubEta[i]->Integral("WIDTH") );                     // NORMALIZE
-  //   hSubEta[i]->SetLineColor( color[i] );    hSubEta[i]->SetMarkerStyle( marker[i] );    hSubEta[i]->SetMarkerColor( color[i] );
-  //   hSubEta[i]->Draw("SAME");                                                    // DRAW
-  //   Ndj = ""; avg = "";    Ndj += hSubEta[i]->GetEntries();
-  //   avg += hSubEta[i]->GetMean(1);                                           // 1 denotes x-axis
-  //   avg = avg(0,6);
-  //   leg1->AddEntry( name, title, "lpf" );                            // ADD TO LEGEND
-  //   leg1->AddEntry((TObject*)0,Ndj, "");    leg1->AddEntry((TObject*)0,avg, "");
-  // }
-
-  // leg1->Draw();  c0->Modified();  c0->cd();  c0->SetSelected(c0);
-  // name = "plots/" + path + "SubEtaDist.pdf";
-  // c0->SaveAs( name , "PDF");
-  
-  // hSubPtEtaPhi->GetXaxis()->SetRangeUser(0.0,100.0);
   
   c0->SetLogy();
   
   name = /*JetChargeString + ": " + BackgroundChargeString + " */ "Underlying Event by Lead Jet p_{T};#rho (GeV);";
-  TH2D *sLeadPtVsRho = new TH2D( "sLeadPtVsRho", name, 50,0,15, 10,0.000001, 1.0 );
+  TH2D *sLeadPtVsRho = new TH2D( "sLeadPtVsRho", name, 50,0,15, 10,0, 1.0 );
   sLeadPtVsRho->SetStats(0);
 
   TLegend *leg2 = new TLegend(0.6, 0.65, 0.9, 0.9,NULL,"brNDC");    // LEGEND
@@ -214,16 +186,14 @@ void HTjetPlot() {
 
   hLeadPtVsRho->GetXaxis()->SetRangeUser(0.0, 10.0);
   hLeadPtVsRho->GetYaxis()->SetRangeUser(0.0, 100.0);
-  
+    
+  // hTowersPerEvent->Draw();		name = "plots/" + path + "TowersPerEvent.pdf";
+  // // title = JetChargeString + ": " + hTowersPerEvent->GetTitle();		hTowersPerEvent->SetTitle( title );
+  // c0->SaveAs( name, "PDF" );
 
-  
-  hTowersPerEvent->Draw();		name = "plots/" + path + "TowersPerEvent.pdf";
-  // title = JetChargeString + ": " + hTowersPerEvent->GetTitle();		hTowersPerEvent->SetTitle( title );
-  c0->SaveAs( name, "PDF" );
-
-  hPrimaryPerEvent->Draw();		name = "plots/" + path + "PrimaryPerEvent.pdf";
-  // title = JetChargeString + ": " + hPrimaryPerEvent->GetTitle();		hPrimaryPerEvent->SetTitle( title );
-  c0->SaveAs( name, "PDF" );
+  // hPrimaryPerEvent->Draw();		name = "plots/" + path + "PrimaryPerEvent.pdf";
+  // // title = JetChargeString + ": " + hPrimaryPerEvent->GetTitle();		hPrimaryPerEvent->SetTitle( title );
+  // c0->SaveAs( name, "PDF" );
 
   TH1D *hAllJetsPt = (TH1D*) hAllJetsPtEtaPhi->ProjectionX();		hAllJetsPt->Scale(1.0/hAllJetsPt->Integral());
   title = "Inclusive Jet p_{T};p_{T} (GeV)";			hAllJetsPt->SetTitle(title);
@@ -242,43 +212,6 @@ void HTjetPlot() {
   title = "Lead Jet #eta-#phi;#eta;#phi";				hLeadEtaPhi->SetTitle( title );				hLeadEtaPhi->GetZaxis()->SetRangeUser(0.00001,0.01);
   hLeadEtaPhi->Draw("COLZ");			name = "plots/" + path + "LeadEtaPhi.pdf";				       	c0->SaveAs( name, "PDF" );
   
-  // TH2D *hAllEtaPhi = (TH2D*) hAllPtEtaPhi->Project3D("ZY");			hAllEtaPhi->Scale(1.0/hAllEtaPhi->Integral());
-  // title = JetChargeString + ": All Particles #eta-#phi;#eta;#phi";			hAllEtaPhi->SetTitle( title );				hAllEtaPhi->GetZaxis()->SetRangeUser(0.000001,0.01);
-  // hAllEtaPhi->Draw("COLZ");			name = "plots/" + path + "AllEtaPhi.pdf";			  	  	       	c0->SaveAs( name, "PDF" );
-    
-  // TH2D *hPartEtaPhi = (TH2D*) hPartPtEtaPhi->Project3D("ZY");			hPartEtaPhi->Scale(1.0/hPartEtaPhi->Integral());
-  // title = JetChargeString + ", " + BackgroundChargeString + ": Background Particles #eta-#phi;#eta;#phi";		hPartEtaPhi->SetTitle( title );
-  // hPartEtaPhi->GetZaxis()->SetRangeUser(0.000001,0.01);
-  // hPartEtaPhi->Draw("COLZ");			name = "plots/" + path + "PartEtaPhi_" + BackgroundChargeBias + ".pdf";			  	     	    	c0->SaveAs( name, "PDF" );
-
-  // if ( BackgroundChargeBias == "allBG" ) {
-  //   hChgVsNeuBG->GetZaxis()->SetRangeUser(0.00001,1);		title = JetChargeString + ": " + hChgVsNeuBG->GetTitle();		hChgVsNeuBG->SetTitle( title );
-  //   hChgVsNeuBG->Draw("COLZ");			name = "plots/" + path + "ChgVsNeuBG.pdf";				c0->SaveAs( name, "PDF" );
-  // }
-  
-  // hnPrimaryVSnTowers->GetZaxis()->SetRangeUser(0.00001,0.01);
-  // title = JetChargeString + ": " + hnPrimaryVSnTowers->GetTitle();			hnPrimaryVSnTowers->SetTitle( title );
-  // hnPrimaryVSnTowers->Draw("COLZ");		name = "plots/" + path + "nPrimaryVSnTowers.pdf";			c0->SaveAs( name, "PDF" );
-
-  // hPrimaryVsBBC->GetZaxis()->SetRangeUser(0.00001,1);
-  // hPrimaryVsBBC->Draw("COLZ");			name = "plots/" + path + "PrimaryVsBBC_" + BackgroundChargeBias + ".pdf";				c0->SaveAs( name, "PDF" );
-
-  // hPrimaryVsGlobal->GetZaxis()->SetRangeUser(0.00001,0.01);
-  // title = JetChargeString + ": " + hPrimaryVsGlobal->GetTitle();			hPrimaryVsGlobal->SetTitle( title );
-  // hPrimaryVsGlobal->Draw("COLZ");		name = "plots/" + path + "PrimaryVsGlobal.pdf";			c0->SaveAs( name, "PDF" );
-
-  // hPrimaryVsRho->GetZaxis()->SetRangeUser(0.00001,1);
-  // title = JetChargeString + ", " + BackgroundChargeString + ": " + hPrimaryVsRho->GetTitle();			hPrimaryVsRho->SetTitle( title );
-  // hPrimaryVsRho->Draw("COLZ");			name = "plots/" + path + "PrimaryVsRho_" + BackgroundChargeBias + ".pdf";				c0->SaveAs( name, "PDF" );
-
-  // hGlobalVsRho->GetZaxis()->SetRangeUser(0.00001,0.01);
-  // title = JetChargeString + ", " + BackgroundChargeString + ": " + hGlobalVsRho->GetTitle();			hGlobalVsRho->SetTitle( title );
-  // hGlobalVsRho->Draw("COLZ");			name = "plots/" + path + "GlobalVsRho_" + BackgroundChargeBias + ".pdf";				c0->SaveAs( name, "PDF" );
-
-  // hTowersVsRho->GetZaxis()->SetRangeUser(0.00001,1);
-  // // title = JetChargeString + ", " + BackgroundChargeString + ": " + hTowersVsRho->GetTitle();			hTowersVsRho->SetTitle( title );
-  // hTowersVsRho->Draw("COLZ");			name = "plots/" + path + "TowersVsRho_" + BackgroundChargeBias + ".pdf";				c0->SaveAs( name, "PDF" );
-
   scale = hLeadPtVsRho->Integral("WIDTH");			hLeadPtVsRho->Scale(1.0/scale);			hLeadPtVsRho->GetZaxis()->SetRangeUser(0.00001,1);
   //title = JetChargeString + ", " + BackgroundChargeString + ": " + hLeadPtVsRho->GetTitle();			hLeadPtVsRho->SetTitle( title );
   hLeadPtVsRho->Draw("COLZ");			name = "plots/" + path + "LeadPtVsRho.pdf";				c0->SaveAs( name, "PDF" );
