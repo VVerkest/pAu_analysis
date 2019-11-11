6305fa03bf0a0d10d77256123996f0ccc2379650
VVerkest <vverkest@gmail.com>
2019-11-11 16:09:24 -0500

    Update UEjetPlot.C

--- macros/UEjetPlot.C
+++ macros/UEjetPlot.C
@@ -89,7 +89,7 @@ void UEjetPlot(){
   hBGneu->Draw("COLZ");
   c0->SaveAs( "plots/UE/neuBgEtaPhi.pdf" , "PDF" );
 
-  hTowersVsRho->GetZaxis()->SetRangeUser(0.000001,1);
+  c0->GetZaxis()->SetRangeUser(0.000001,1);
   hTowersVsRho->Scale(1./hTowersVsRho->Integral("WIDTH"));
   hTowersVsRho->Draw("COLZ");
   c0->SaveAs( "plots/UE/towersVsRho.pdf" , "PDF" );
