17dd455e790a64e7ebccfb8b5b97dcaa0a10046e
VVerkest <vverkest@gmail.com>
2019-11-11 19:08:57 -0500

    Update UEjetPlot.C

--- macros/UEjetPlot.C
+++ macros/UEjetPlot.C
@@ -207,7 +207,7 @@ void UEjetPlot(){
 
 
   jetTree->Draw("leadPt:((chgEastRho+neuEastRho)+(chgMidRho+neuMidRho)+(chgWestRho+neuWestRho))/3>>hRho2d","","COLZ");
-  TH2D *hRho2d = (TH1F*)gDirectory->Get("hRho2d");
+  TH2D *hRho2d = (TH2D*)gDirectory->Get("hRho2d");
   c1->SetLogy();
   TLegend *leg2 = new TLegend(0.65, 0.65, 0.9, 0.9,NULL,"brNDC");    // LEGEND 0
   leg2->SetBorderSize(1);   leg2->SetLineColor(1);   leg2->SetLineStyle(1);   leg2->SetLineWidth(1);   leg2->SetFillColor(0);   leg2->SetFillStyle(1001);
