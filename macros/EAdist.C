3ec904cd338005fdb6a9fed60330a5c3def2ebd7
VVerkest <vverkest@gmail.com>
2020-02-14 18:05:24 -0500

    Update EAdist.C

--- macros/EAdist.C
+++ macros/EAdist.C
@@ -6,20 +6,20 @@ void EAdist(){
   TFile *EAfile = new TFile("out/EAdist/pAu_2015_EAdist.root","READ");
 
   TH1D *hBBCdist = (TH1D*) EAfile->Get("hEAdist");
-  hEAdist->Scale(1./hEAdist->Integral());
+  hBBCdist->Scale(1./hBBCdist->Integral());
   
   TTree *eatree = (TTree*) EAfile->Get("EAtree");
 
   int RunID, EventID;		double Vz, BbcAdcSumEast, ps;
-  EAtree->SetBranchAddress( "RunID", &RunID );	EAtree->SetBranchAddress( "EventID", &EventID );
-  EAtree->SetBranchAddress( "Vz", &Vz );		EAtree->SetBranchAddress( "BbcAdcSumEast", &BbcAdcSumEast );
+  eatree->SetBranchAddress( "RunID", &RunID );	eatree->SetBranchAddress( "EventID", &EventID );
+  eatree->SetBranchAddress( "Vz", &Vz );		eatree->SetBranchAddress( "BbcAdcSumEast", &BbcAdcSumEast );
 
   int nBins = hEAdist->GetNbinsX();
   TH1D *hBBCint = new TH1D("hBBCint","BBCEsum Dis. Integral",70000,0,70000);
 
   for (int i=0;i<nBins;++i) {
-    ps = hEAdist->Integral(0,i);  //partial sum
-    hEAint->SetBinContent(i,ps);
+    ps = hBBCdist->Integral(0,i);  //partial sum
+    hBBCint->SetBinContent(i,ps);
   }
 
 }
