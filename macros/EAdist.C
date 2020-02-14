aa888b8df24c0fba8517df8cf1f10a1f610c2298
VVerkest <vverkest@gmail.com>
2020-02-14 18:35:57 -0500

    print deciles into file

--- macros/EAdist.C
+++ macros/EAdist.C
@@ -10,16 +10,36 @@ void EAdist(){
   
   TTree *eatree = (TTree*) EAfile->Get("EAtree");
 
-  int RunID, EventID;		double Vz, BbcAdcSumEast, ps;
+  int RunID, EventID;		double Vz, BbcAdcSumEast, ps, tenth;
   eatree->SetBranchAddress( "RunID", &RunID );	eatree->SetBranchAddress( "EventID", &EventID );
   eatree->SetBranchAddress( "Vz", &Vz );		eatree->SetBranchAddress( "BbcAdcSumEast", &BbcAdcSumEast );
 
   int nBins = hBBCdist->GetNbinsX();
-  TH1D *hBBCint = new TH1D("hBBCint","BBCEsum Dis. Integral",70000,0,70000);
+  TH1D *hBBCint = new TH1D("hBBCint","BBCEsum Distribution Integral",70000,0,70000);
 
   for (int i=0;i<nBins;++i) {
     ps = hBBCdist->Integral(0,i);  //partial sum
     hBBCint->SetBinContent(i,ps);
   }
 
+  double tenth = 0.10000000000000;
+  double deciles[10];
+  for (int i=0;i<10;++i) {
+    double min = (i+1)*tenth;
+    int bin = hBBCint->FindFirstBinAbove(min);
+    deciles[i] = hBBCint->GetBinCenter(bin);
+  }
+
+  fstream file("src/EAdeciles.txt", fstream::in | fstream::out | fstream::trunc);
+  if (!file) {    cerr << "Error in creating file!!!" << endl; exit(1);  }
+  else {    cout << "src/EAdeciles.txt created successfully." << endl;  }
+  
+  file << 0.00 << ", ";
+  for (int i=0;i<9;++i) {file << deciles[i] << ", ";}
+  file << deciles[10]; //writes that element
+  
+  file.close();
+  cout << "Closed bad_towers_pAu2015.list" << endl;
+
+  
 }
