43099121cb87e9a13285f6d253ed64dc152c05c6
VVerkest <vverkest@gmail.com>
2020-02-14 20:24:25 -0500

    debug output

--- macros/EAdist.C
+++ macros/EAdist.C
@@ -18,8 +18,7 @@ void EAdist(){
   int nBins = hBBCdist->GetNbinsX();
   TH1D *hBBCint = new TH1D("hBBCint","BBCEsum Distribution Integral",280000,0,70000);
 
-  // for (int i=0;i<nBins;++i) {
-  for (int i=0;i<10000;++i) {
+  for (int i=0;i<nBins;++i) {
     ps = hBBCdist->Integral(0,i);  //partial sum
     hBBCint->SetBinContent(i,ps);
   }
@@ -27,6 +26,7 @@ void EAdist(){
   double tenth = 0.10000000000000;
   double deciles[10];
   for (int i=0;i<10;++i) {
+    cout<<"Finding Decile: " + i + "0-" + (i+1) + "0%"<<endl;
     double min = (i+1)*tenth;
     int bin = hBBCint->FindFirstBinAbove(min);
     deciles[i] = hBBCint->GetBinCenter(bin);
