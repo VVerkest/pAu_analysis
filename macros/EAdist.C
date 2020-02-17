2716f6a45e550af4387e1d3b25502dace35cf178
VVerkest <vverkest@gmail.com>
2020-02-17 15:42:38 -0500

    fix decile lines

--- macros/EAdist.C
+++ macros/EAdist.C
@@ -29,15 +29,12 @@ void EAdist(){
   }
 
   double tenth = (double) 1.0/10.0;
-  cout<<tenth;
   double deciles[10];
   for (int i=0;i<10;++i) {
     double min = (i+1)*tenth;
-    //cout<<min<<", ";
     int bin = hBBCint->FindFirstBinAbove(min);
     deciles[i] = hBBCint->GetBinCenter(bin);
-    //cout<<deciles[i]<<", ";
-    hBBCdist->SetBinError(i,9999);
+    hBBCdist->SetBinError(bin,9999);
   }
 
   fstream file("src/EAdeciles.txt", fstream::in | fstream::out | fstream::trunc);
@@ -46,14 +43,14 @@ void EAdist(){
   
   file << 0.00 << ", ";
   for (int i=0;i<9;++i) {file << deciles[i] << ", ";}
-  file << 64000; //writes that element
+  file << 64000;
   
   file.close();
   cout << "Closed bad_towers_pAu2015.list" << endl;
 
   TCanvas * c0 = new TCanvas( "c0" , "" ,0 ,23 ,1280 ,350 );              // CANVAS
   c0->SetLogy();
-  hBBCdist->GetYaxis()->SetRangeUser(0.000001, 0.0001);
+  hBBCdist->GetYaxis()->SetRangeUser(0.000001, 0.01);
   hBBCdist->Rebin(200);
   hBBCdist->Draw();
   c0->SaveAs("plots/EAdist.pdf","PDF");
