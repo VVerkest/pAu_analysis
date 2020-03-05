34ec462eb40fdaab6b790865c8a56f75d8d50d16
VVerkest <vverkest@gmail.com>
2020-03-05 02:44:33 -0500

    initialize value before loop

--- src/pAuFunctions.cxx
+++ src/pAuFunctions.cxx
@@ -38,8 +38,8 @@ namespace pAuAnalysis {
 	phi = uncorrPart[i].phi();
 	e = uncorrPart[i].e();
 
+	etaBin = 999;
 	for ( int j=0; j<nBins; ++j ) {     // find efficiency histogram corresponding to track eta
-	  etaBin = 999;
 	  if ( (eta>=etaLo[j]) && (eta<=etaHi[j]) ) { etaBin = j; continue; }
 	}
 	if ( etaBin==999 ) { std::cerr<<"error finding charged track eta bin; eta = "<<eta<<std::endl; continue; }
