a09736c95a1f5fda100a53ce35eaec0c8f0ca5b6
VVerkest <vverkest@gmail.com>
2020-05-03 17:54:32 -0400

    fix trigger selection; require trigger to be in lead or reco jet

--- src/pAuHTdijetUE.cxx
+++ src/pAuHTdijetUE.cxx
@@ -25,9 +25,9 @@ int main ( int argc, const char** argv ) {         // funcions and cuts specifie
   double chgEastSum, chgMidSum, chgWestSum, neuEastSum, neuMidSum, neuWestSum;
   
   //  Tree variables
-  int RunID, EventID, nTowers, nPrimary, nGlobal, nVertices, refMult, gRefMult, eval, pval, nBGpart_chg, nBGpart_neu;
+  int RunID, EventID, nTowers, nPrimary, nGlobal, nVertices, refMult, gRefMult, eval, pval, nBGpart_chg, nBGpart_neu, nHTtrig;
   double Vz, BbcAdcSumEast, BbcAdcSumEastOuter, BbcAdcSumWest, BbcAdcSumWestOuter, leadPt, recoPt, leadEta, recoEta, leadPhi, recoPhi,
-    chgEastRho, chgMidRho, chgWestRho, neuEastRho, neuMidRho, neuWestRho, leadArea, recoArea;
+    chgEastRho, chgMidRho, chgWestRho, neuEastRho, neuMidRho, neuWestRho, leadArea, recoArea, dRTrigLead, dRTrigReco;
 
   HTdijetTree->Branch( "RunID", &RunID );
   HTdijetTree->Branch( "EventID", &EventID );
@@ -58,6 +58,7 @@ int main ( int argc, const char** argv ) {         // funcions and cuts specifie
   HTdijetTree->Branch( "recoPhi", &recoPhi );
   HTdijetTree->Branch( "nBGpart_chg", &nBGpart_chg );
   HTdijetTree->Branch( "nBGpart_neu", &nBGpart_neu );
+  HTdijetTree->Branch( "nHTtrig", &nHTtrig );
   
   TH3D *hChgBgPtEtaPhi = new TH3D( "hChgBgPtEtaPhi", "Charged Background #phi vs. #eta;p_{T} (GeV);#eta;#phi", 40,0,20, 40,-1.0,1.0, 120,0.0,2*pi );
   TH3D *hNeuBgPtEtaPhi = new TH3D( "hNeuBgPtEtaPhi", "Neutral Background #phi vs. #eta;p_{T} (GeV);#eta;#phi", 40,0,20, 40,-1.0,1.0, 120,0.0,2*pi );
@@ -90,10 +91,16 @@ int main ( int argc, const char** argv ) {         // funcions and cuts specifie
     event = Reader.GetEvent();
     header = event->GetHeader();
     container = Reader.GetOutputContainer();
+
+    if (header->GetRunId() >= 16142059 && header->GetRunId() <= 16149001) { continue; }    //TEMPORARILY SKIPPING THESE RUNS
+    if (header->GetRunId() == 16135031 || header->GetRunId() == 16135032) { continue; }
+    if ( abs(Vz) > vzCut ) { continue; }
+    if (!(header->HasTriggerId(500205) || header->HasTriggerId(500215))) {continue;}   //  ONLY SELECT HT TRIGGER EVENTS
+    if ( header->GetBbcAdcSumEast() > 64000 ) { continue; }
+    if ( header->GetBbcAdcSumEast() < 3559.12 ) { continue; }     //  neglect 0-10% event activity
     
     Vz = header->GetPrimaryVertexZ();
-    if ( UseHTevent( header, event, vzCut, Vz ) == false ) { continue; } // Skip events based on: Run#, vz cut, BBCSumE; only accept HT events
-    if ( header->GetBbcAdcSumEast() < 3559.12 ) { continue; }     //  neglect 0-10% event activity
+    //if ( UseHTevent( header, event, vzCut, Vz ) == false ) { continue; } // Skip events based on: Run#, vz cut, BBCSumE; only accept HT events
 
     GatherParticles( container, rawParticles );
 
@@ -103,86 +110,122 @@ int main ( int argc, const char** argv ) {         // funcions and cuts specifie
 
     leadJetSelector = leadPtMinSelector && ptMaxSelector && jetEtaSelector;
     rawJets = sorted_by_pt( leadJetSelector( jetCluster.inclusive_jets() ) );     // EXTRACT ALL JETS IN 10-30 GeV RANGE
-    if ( rawJets.size()>0 ) { leadJet = rawJets[0]; }
-    else { continue; }
-
-    for ( int p=0; p<3; ++p ) {
-      if ( leadJet.pt() >= ptLo[p]  &&  leadJet.pt() <= ptHi[p] ) { pval = p; }
-    }
-    for ( int e=0; e<3; ++e ) {
-      if ( leadJet.eta() >= etaLo[e]  &&  leadJet.eta() <= etaHi[e] ) { eval = e; }
-    }
-    if ( pval==99 || eval==99 ) { cerr<<"UNABLE TO FIND PT OR ETA RANGE FOR LEAD JET"<<endl; }
     
+    if ( rawJets.size()>0 ) {
+      leadJet = rawJets[0]; 
 
-    //   REQUIRE RECOIL JET TO HAVE AT LEAST HALF OF LEAD PT!
-    Selector recoPtRangeSelector = SelectorPtRange( leadJet.pt()/2, ptHi[pval] );          //  JET pT RANGE    { 10-15, 15-20, 20-30 }
-    Selector etaRangeSelector = SelectorEtaRange( etaLo[eval], etaHi[eval] );          //  JET eta RANGE
-
-    // Selector recoJetSelector = recoPtRangeSelector && etaRangeSelector && jetEtaSelector;
-    Selector recoJetSelector = recoPtRangeSelector && jetEtaSelector;
-    
-    recoCandidates = sorted_by_pt( recoJetSelector( jetCluster.inclusive_jets() ) );     // EXTRACT SELECTED JETS
-
-    
-    bool hasReco = false;
-    
-    if ( recoCandidates.size()>0 ) {
-
-      for ( int i=0; i<recoCandidates.size(); ++i ) {
-	double deltaPhi_abs = fabs( recoCandidates[i].phi() - leadJet.phi() );
-	if ( fabs( deltaPhi_abs - pi ) <= R ) {
-	  recoJet = recoCandidates[i];
-       	  hasReco = true;
-	}
-	if ( hasReco == true ) { continue; }  // exit loop with highest-pt jet in recoil range
+      int trigTowId;
+      TStarJetPicoTriggerInfo *trig;
+      TStarJetPicoTower *tow, *triggerTower;
+      double trigTowEt = 0.0;
+      std::vector<int> trigTowers;
+      for ( int i=0; i<event->GetTrigObjs()->GetEntries(); ++i ) {
+	trig = (TStarJetPicoTriggerInfo *)event->GetTrigObj(i);
+	if ( trig->isBHT2() && UseTriggerTower( trig->GetId()) ) { trigTowers.push_back( trig->GetId() ); }
       }
+      sort(trigTowers.begin(), trigTowers.end());
 
+      int nmatched = 0;
+      for (int i=0; i<nTowers; ++i){				// loop throught selected towers in event
+	tow = (TStarJetPicoTower*)SelectedTowers->At(i);
+	if ( tow->GetEt()>=5.4 && count(trigTowers.begin(), trigTowers.end(), tow->GetId())) { // min 5.4 GeV tower and must be in list of HT towers
+
+	  towPJ.reset_PtYPhiM( tow->GetEt(), tow->GetEta(), tow->GetPhi(), 0.0 ); //reset_PtYPhiM!!
+	  deltaPhi = fabs( leadJet.delta_phi_to( towPJ ) );
+	  deltaR = leadJet.delta_R( towPJ );
+
+	  if ( deltaR<=R || fabs(deltaPhi)>=(pi-R) ) {  // require trigger
+	    if ( nmatched>0 && (tow->GetEt()<trigTowEt) ) { continue; }   // more than 1 trigger tower
+	    else {							// first trigger tower
+	      trigTowEt = tow->GetEt();
+	      trigTowerPJ.reset_PtYPhiM( tow->GetEt(), tow->GetEta(), tow->GetPhi(), 0.0 ); //reset_PtYPhiM!!
+	      nmatched += 1;
+	    }
+	  }
+	  
+	}
+      }
+      if (nmatched>0) {
+	nHTtrig = nmatched;
+
+      
+	//   REQUIRE RECOIL JET TO HAVE AT LEAST HALF OF LEAD PT!
+	Selector recoPtRangeSelector = SelectorPtRange( leadJet.pt()/2, ptHi[pval] );          //  JET pT RANGE    { 10-15, 15-20, 20-30 }
+	Selector etaRangeSelector = SelectorEtaRange( etaLo[eval], etaHi[eval] );          //  JET eta RANGE
+
+	// Selector recoJetSelector = recoPtRangeSelector && etaRangeSelector && jetEtaSelector;
+	Selector recoJetSelector = recoPtRangeSelector && jetEtaSelector;
+    
+	recoCandidates = sorted_by_pt( recoJetSelector( jetCluster.inclusive_jets() ) );     // EXTRACT SELECTED JETS
+
+    
+	bool hasReco = false;
+    
+	if ( recoCandidates.size()>0 ) {
+
+	  for ( int i=0; i<recoCandidates.size(); ++i ) {
+	    double deltaPhi_abs = fabs( recoCandidates[i].phi() - leadJet.phi() );
+	    if ( fabs( deltaPhi_abs - pi ) <= R ) {
+	      recoJet = recoCandidates[i];
+	      hasReco = true;
+	    }
+	    if ( hasReco == true ) { continue; }  // exit loop with highest-pt jet in recoil range
+	  }
+
+	}
+	else { continue; }
+	
+	if ( hasReco == false ) { continue; }
+	else {
+
+	  dRTrigLead = leadJet.delta_R( trigTowerPJ );
+	  dRTrigReco = recoJet.delta_R( trigTowerPJ );
+
+	  if ( dRTrigLead<=R || dRTrigReco<=R ) {
+	  
+	    RunID = header->GetRunId();
+	    EventID = Reader.GetNOfCurrentEvent();
+	    TList *SelectedTowers = Reader.GetListOfSelectedTowers();
+	    nTowers = CountTowers( SelectedTowers );
+	    nPrimary = header->GetNOfPrimaryTracks();
+	    nGlobal = header->GetNGlobalTracks();
+	    nVertices = header->GetNumberOfVertices();
+	    refMult = header->GetReferenceMultiplicity();
+	    gRefMult = header->GetGReferenceMultiplicity();
+	    BbcAdcSumEast = header->GetBbcAdcSumEast();
+	    BbcAdcSumEastOuter = header->GetBbcAdcSumEastOuter();
+	    BbcAdcSumWest = header->GetBbcAdcSumWest();
+	    BbcAdcSumWestOuter = header->GetBbcAdcSumWestOuter();
+	    leadPt = leadJet.pt();
+	    leadEta = leadJet.eta();
+	    leadPhi = leadJet.phi();
+	    leadArea = leadJet.area();
+	    recoPt = recoJet.pt();
+	    recoEta = recoJet.eta();
+	    recoPhi = recoJet.phi();
+	    recoArea = recoJet.area();
+
+	    //  BACKGROUND ESTIMATION
+	    GatherChargedBG( leadJet, container, chgParticles );   // gather BG
+	    GatherNeutralBG( leadJet, container, neuParticles );
+    
+	    nBGpart_chg = chgParticles.size();
+	    nBGpart_neu = neuParticles.size();
+
+	    chgEastSum = 0;            chgMidSum = 0;            chgWestSum = 0;            neuEastSum = 0;            neuMidSum = 0;            neuWestSum = 0;
+	    CalculateRhoByChargeAndEta(chgParticles,neuParticles,chgEastSum,chgMidSum,chgWestSum,neuEastSum,neuMidSum,neuWestSum,hChgBgPtEtaPhi,hNeuBgPtEtaPhi);
+	    chgEastRho = chgEastSum/eastArea;
+	    chgMidRho = chgMidSum/midArea;
+	    chgWestRho = chgWestSum/westArea;
+	    neuEastRho = neuEastSum/eastArea;
+	    neuMidRho = neuMidSum/midArea;
+	    neuWestRho = neuWestSum/westArea;
+    
+	    HTdijetTree->Fill();
+	  }
+	}
+      }
     }
-    else { continue; }
-    if ( hasReco == false ) { continue; }
-
-    
-    RunID = header->GetRunId();
-    EventID = Reader.GetNOfCurrentEvent();
-    TList *SelectedTowers = Reader.GetListOfSelectedTowers();
-    nTowers = CountTowers( SelectedTowers );
-    nPrimary = header->GetNOfPrimaryTracks();
-    nGlobal = header->GetNGlobalTracks();
-    nVertices = header->GetNumberOfVertices();
-    refMult = header->GetReferenceMultiplicity();
-    gRefMult = header->GetGReferenceMultiplicity();
-    BbcAdcSumEast = header->GetBbcAdcSumEast();
-    BbcAdcSumEastOuter = header->GetBbcAdcSumEastOuter();
-    BbcAdcSumWest = header->GetBbcAdcSumWest();
-    BbcAdcSumWestOuter = header->GetBbcAdcSumWestOuter();
-    leadPt = leadJet.pt();
-    leadEta = leadJet.eta();
-    leadPhi = leadJet.phi();
-    leadArea = leadJet.area();
-    recoPt = recoJet.pt();
-    recoEta = recoJet.eta();
-    recoPhi = recoJet.phi();
-    recoArea = recoJet.area();
-
-    //  BACKGROUND ESTIMATION
-    GatherChargedBG( leadJet, container, chgParticles );   // gather BG
-    GatherNeutralBG( leadJet, container, neuParticles );
-    
-    nBGpart_chg = chgParticles.size();
-    nBGpart_neu = neuParticles.size();
-
-    chgEastSum = 0;            chgMidSum = 0;            chgWestSum = 0;            neuEastSum = 0;            neuMidSum = 0;            neuWestSum = 0;
-    CalculateRhoByChargeAndEta(chgParticles,neuParticles,chgEastSum,chgMidSum,chgWestSum,neuEastSum,neuMidSum,neuWestSum,hChgBgPtEtaPhi,hNeuBgPtEtaPhi);
-    chgEastRho = chgEastSum/eastArea;
-    chgMidRho = chgMidSum/midArea;
-    chgWestRho = chgWestSum/westArea;
-    neuEastRho = neuEastSum/eastArea;
-    neuMidRho = neuMidSum/midArea;
-    neuWestRho = neuWestSum/westArea;
-    
-    HTdijetTree->Fill();
-    
   }  // ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~  END EVENT LOOP!  ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
 
   TFile *pAuFile = new TFile( outFile.c_str() ,"RECREATE");
