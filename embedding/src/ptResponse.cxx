a2ceeee6e21923e902d76b6ca601519781edeecd
VVerkest <vverkest@gmail.com>
2020-08-31 17:13:26 -0400

    produces all necessary plots now!

--- embedding/src/ptResponse.cxx
+++ embedding/src/ptResponse.cxx
@@ -29,6 +29,11 @@ int main () {
 
   TString name, title;
 
+
+  // ########################################################################################################
+  //                           EMBEDDING FILE FOR LEADING JET PT CORRECTION
+  // ########################################################################################################
+  
   // OPEN RESPONSE FILE AND COLLECT HISTOS
   TFile *inFile = new TFile("out/sim/pAu2015embedding.root","READ");
 
@@ -45,16 +50,12 @@ int main () {
 
     nAccepted += hResponse[e]->GetEntries();
     nFakes += hFakes[e]->GetEntries();
-
   }
 
   TH2D* hPtResponse = new TH2D("hPtResponse",";part-level leading jet p_{T} (GeV);det-level leading jet p_{T} (GeV)",55,4.5,59.5, 55,4.5,59.5);
   TH1D* hFakeJets = new TH1D("hFakeJets",";missing part-level leading jet p_{T} (GeV)",55,4.5,59.5);
   
-  for (int e=0; e<nEtaBins; ++e) {    
-    hPtResponse->Add(hResponse[e]);
-    hFakeJets->Add(hFakes[e]);
-  }
+  for (int e=0; e<nEtaBins; ++e) {     hPtResponse->Add(hResponse[e]);     hFakeJets->Add(hFakes[e]);   }
 
   TCanvas * can1 = new TCanvas( "can1" , "" ,700 ,500 );              // CANVAS 1
   can1->SetLogz();
@@ -69,29 +70,30 @@ int main () {
   hMissedJets->Draw();
   can2->SaveAs("plots/test/MissedJets.pdf","PDF");
 
-  TCanvas * can = new TCanvas( "can" , "" ,700 ,500 );              // CANVAS 0
-  can->SetLogz();
+  TCanvas * can3 = new TCanvas( "can3" , "" ,700 ,500 );              // CANVAS 3
+  can3->SetLogz();
   hPtResponse->Draw("COLZ");
-  can->SaveAs("plots/test/pTresponse.pdf","PDF");  // SAVE 2D PT RESPONSE
+  can3->SaveAs("plots/test/pTresponse.pdf","PDF");  // SAVE 2D PT RESPONSE
   
   TH1D *hDet[55]; TH1D *hDetWt[nPtBins];
 
   for (int p=0; p<nPtBins; ++p) {
-    TString name = "hPtResponse"; name += ptBinName[p];
+    name = "hPtResponse"; name += ptBinName[p];
     hDetWt[p] = new TH1D( name, ";det-level leading jet p_{T} (GeV)",55,4.5,59.5);
   }
 
+  TCanvas * can4 = new TCanvas( "can4" , "" ,700 ,500 );              // CANVAS 4
   
   for (int i=5; i<26; ++i) {  // PROJECT FOR ALL PART-LEVEL BINS 10-30 GeV
     int ptVal = i + 5;
     int binno = i + 1;
     
-    TString name = "hPtResponse_"; name += ptVal; name += "GeV";
+    name = "hPtResponse_"; name += ptVal; name += "GeV";
     hDet[i] = (TH1D*) hPtResponse->ProjectionY(name,binno,binno);
     hDet[i]->Scale(1./hDet[i]->Integral());
-    hDet[i]->Draw();
+    hDet[i]->SetMarkerStyle(marker[i]);
+    hDet[i]->Draw("SAME PLC PMC");
     name = "plots/test/PtResponse_"; name += ptVal; name += "GeV"; name += ".pdf";
-    can->SaveAs( name, "PDF" );
     hDet[i]->SetMarkerStyle(marker[i]);
     hDet[i]->SetMarkerSize(1);
     hDet[i]->SetStats(0);
@@ -99,10 +101,10 @@ int main () {
     hDet[i]->SetNameTitle(name,";det-level leading jet p_{T} (GeV)");
   }
   
-  hDet[5]->Draw("PLC PMC");
-  for (int i=6; i<26; ++i) { hDet[i]->Draw("SAME PLC PMC"); }
-  can->BuildLegend(0.68,0.1,0.9,0.9);
-  can->SaveAs("plots/test/detPtResponses.pdf","PDF");
+  can4->BuildLegend(0.68,0.1,0.9,0.9);
+  can4->SaveAs("plots/test/detPtResponses.pdf","PDF");
+
+  TCanvas * can5 = new TCanvas( "can5" , "" ,700 ,500 );              // CANVAS 5
 
   for (int p=0; p<nPtBins; ++p) {  //  WEIGHT AND ADD PROJECTIONS TO GET WEIGHTED DETECTOR RESPONSE
     for (int i=5; i<26; ++i) {
@@ -113,11 +115,152 @@ int main () {
 	hDetWt[p]->Add(hDet[i],wt);
       }
     }    
-    TString name = "plots/test/WeightedPtResponse"; name += ptBinName[p]; name += ".pdf";
     hDetWt[p]->Scale(1./hDetWt[p]->Integral());
-    hDetWt[p]->Draw();
-    can->SaveAs(name,"PDF");
+    hDetWt[p]->GetYaxis()->SetRangeUser(0.0,0.2);
+    hDetWt[p]->SetMarkerStyle(ptMarker[p]);
+    hDetWt[p]->Draw("SAME PLC PMC");
+  }
+  can5->BuildLegend(0.4,0.68,0.9,0.9);
+  can5->SaveAs("plots/test/WeightedPtResponse.pdf","PDF");
+
+
+
+  // ########################################################################################################
+  //                                 pAu DATA FILE FOR UE HISTOGRAMS
+  // ########################################################################################################
+
+  // OPEN pAu FILE AND GATHER HISTOGRAMS OF UE AND LEAD PT
+  TFile *UEfile = new TFile("../out/UE/pAuHTjetUE_allEA.root","READ");
+
+  TH1D *hLeadJetPt[nEtaBins];
+  TH3D *hUE3D[nEtaBins];
+  
+  for (int e=0; e<nEtaBins; ++e) {
+  
+    name = "hLeadPt" + etaBinName[e] + "Jet";
+    hLeadJetPt[e] = (TH1D*)UEfile->Get(name);
+
+    name = "hChgUE" + etaBinName[e] + "Jet";
+    hUE3D[e] = (TH3D*)UEfile->Get(name);    
   }
 
+  TH3D *hChgUE3D = new TH3D("hChgUE3D",";chg. UE part. p_{T} (GeV);chg. UE part. #eta", 55,4.5,59.5, 30,0.0,30.0, 40,-1.0,1.0);
+  TH1D *hleadPt = new TH1D("hleadPt",";leading jet p_{T} (GeV)", 55,4.5,59.5);
+
+  for (int e=0; e<nEtaBins; ++e) {     hChgUE3D->Add(hUE3D[e]);     hleadPt->Add(hLeadJetPt[e]);   }
+
+  can5->SetLogy(0);
+  can5->SetLogz();
+  
+  // SAVE 2D HISTO OF UE PT vs. LEADING JET PT
+  TH2D *hChgUE2D = (TH2D*)hChgUE3D->Project3D("YX");
+  hChgUE2D->GetXaxis()->SetRangeUser(0.0,60.0);  hChgUE2D->GetYaxis()->SetRangeUser(0.0,15.0);
+  hChgUE2D->GetYaxis()->SetTitleOffset(1.25);
+  hChgUE2D->Draw("COLZ");
+  can5->SaveAs("plots/test/hChgUE2D.pdf","PDF");
+  hChgUE2D->GetXaxis()->SetRangeUser(4.5,59.5);  hChgUE2D->GetYaxis()->SetRangeUser(0.0,30.0);
+ 
+
+  TH1D *hUEpt[55];  TH1D *hWtUEpt[nPtBins];
+
+  for (int p=0; p<nPtBins; ++p) {
+    name = "hWtUEpt"; name += ptBinName[p];
+    TString title = ptBinString[p]; title += ";ch UE p_{T} (GeV)";
+    hWtUEpt[p] = new TH1D(name,title,30,0.0,30.0);
+  }
+
+  TCanvas * can6 = new TCanvas( "can6" , "" ,700 ,500 );              // CANVAS 6
+
+  can6->SetLogy();
+  for (int i=0; i<55; ++i) {  // det-level fractional pT contribution to part-level pT --> fc(pT_det)
+    int ptVal = i + 5;
+    int binno = i + 1;
+    name = "hUEpt_"; name += ptVal; name += "GeV";
+    hUEpt[i] = (TH1D*)hChgUE2D->ProjectionY(name,binno,binno);
+    if ( (int)hleadPt->GetBinCenter(binno) != ptVal ) { cerr<<"failed pT matching"<<endl; }
+    int nJets = hleadPt->GetBinContent( binno );
+    if (nJets>0){
+      hUEpt[i]->Scale(1./nJets);
+      hUEpt[i]->SetAxisRange( 0.00001,10, "Y");
+      hUEpt[i]->Draw("SAME PLC PMC");
+      
+      for (int p=0; p<nPtBins; ++p) {  // ADD UE DISTRIBUTIONS WEIGHTED BY DET-LEVEL FRACTIONAL CONTRIBUTION
+	double wt = hDetWt[p]->GetBinContent(binno)*( 1.0 - hFakeJets->GetBinContent(binno) );
+	hWtUEpt[p]->Add( hUEpt[i], wt );
+      }
+      hUEpt[i]->SetMarkerStyle(marker[i]);
+      hUEpt[i]->SetMarkerSize(1);
+      hUEpt[i]->SetStats(0);
+      name = ""; name += ptVal; name += " GeV det. jet";
+      hUEpt[i]->SetNameTitle(name,";chg. UE particle p_{T} (GeV)");
+    }
+  }
+  can6->BuildLegend(0.68,0.1,0.9,0.9);
+  can6->SaveAs("plots/test/UEptByLeadPt.pdf","PDF");
+
+  TCanvas * can7 = new TCanvas( "can7" , "" ,700 ,500 );              // CANVAS 7
+  for (int p=0; p<nPtBins; ++p) {
+    hWtUEpt[p]->SetMarkerStyle(ptMarker[p]);
+    hWtUEpt[p]->Draw("PLC PMC SAME");
+    // hWtUEpt[p]->;
+  }
+  can7->SetLogy();
+  can7->BuildLegend(0.4,0.68,0.9,0.9);
+  can7->SaveAs("plots/test/weightedUEptByLeadPt.pdf","PDF");
+
+
+  
+  // ########################################################################################################
+  //                              TRACKING EFFICIENCY FILE FOR UE pT CORRECTION
+  // ########################################################################################################
+  
+  TFile *ef = new TFile( "src/trackeffic_allEta.root", "READ" );
+
+  TH1D *hEffic = (TH1D*)ef->Get( "eff_s_bin_1_10_bbc__1_10_eta" );
+
+  TCanvas * can8 = new TCanvas( "can8" , "" ,700 ,500 );              // CANVAS 8
+  can8->SetLogy();
+  
+  double pt, effic, corrPt, corrErr;
+  int ptBin;
+  for (int p=0; p<nPtBins; ++p) {
+
+    for ( int i=1; i<=hWtUEpt[p]->GetNbinsX(); ++i ) {
+
+      pt = hWtUEpt[p]->GetBinCenter(i);  // loop over chg UE pT bins
+      if ( pt > 3.0 ) { pt = 3.0; }
+      ptBin = hEffic->FindBin( pt );    // find histogram bin corresponding to track pt
+      effic = hEffic->GetBinContent( ptBin );
+      corrPt = hWtUEpt[p]->GetBinContent(i)/effic;                     // calculate corrected bin content and error
+      corrErr = hWtUEpt[p]->GetBinError(i)/effic;                      // (divide bin content and error by efficiency)
+
+      hWtUEpt[p]->SetBinContent( i, corrPt );
+      hWtUEpt[p]->SetBinError( i, corrErr );
+    }
+
+    hWtUEpt[p]->GetXaxis()->SetRangeUser(0.0,15.0);
+    hWtUEpt[p]->SetLineColor(kBlack);
+    hWtUEpt[p]->SetMarkerColor(kBlack);
+    hWtUEpt[p]->Draw();
+
+    TString text = "#LT p_{T}^{ch}#GT = "; text += hWtUEpt[p]->GetMean(1);
+    text = text(0,26);
+    drawText(text, 0.6, 0.7, 20);
+
+    text = "#LT#frac{dN_{ch}}{d#eta d#phi}#GT = "; text += hWtUEpt[p]->Integral()/AREA;
+    text = text(0,42);
+    drawText( text, 0.6, 0.55, 20 );
+      
+    name = "plots/test/CorrectedWtUEpt"; name += ptBinName[p]; name += ".pdf";
+    can8->SaveAs(name,"PDF");
+  }
+
+  hMissedJets->Draw();
+  can8->SaveAs("plots/test/missedJets.pdf","PDF");
+  hFakeJets->Draw();
+  can8->SaveAs("plots/test/fakeJets.pdf","PDF");
+
+  
+  return 0;
 
 }
