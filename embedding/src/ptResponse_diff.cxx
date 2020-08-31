58c6fb8ba78e80c19a0d27ea0c5270d991709db2
VVerkest <vverkest@gmail.com>
2020-08-31 15:23:39 -0400

    move macro... need to compile with makefile

--- embedding/src/ptResponse_diff.cxx
+++ embedding/src/ptResponse_diff.cxx
@@ -18,7 +18,10 @@
 #include <iostream>
 #include <sstream>
 
+#include "params.hh"
+
 using namespace std;
+using namespace Analysis;
 
 int main () {
 
@@ -26,24 +29,6 @@ int main () {
 
   TString name, title;
   
-  const int nPtBins = 3;
-  const double ptLo[nPtBins] = { 10.0, 15.0, 20.0 };
-  const double ptHi[nPtBins] = { 15.0, 20.0, 30.0 };
-  const TString ptBinName[nPtBins] = { "_10_15GeV", "_15_20GeV", "_20_30GeV" };
-  const TString ptBinString[nPtBins] = { "10<p_{T}^{lead}<15", "15<p_{T}^{lead}<20",  "20<p_{T}^{lead}<30" };
-  const int ptMarker[nPtBins] = { 20, 34, 33 };
-
-  //const double AREA = 4*(1.14159265);
-  const int marker[55] = { 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29 };
-  
-  const int nEtaBins = 3;
-  const double etaLo[nEtaBins] = { -1.0, -0.3, 0.3 };
-  const double etaHi[nEtaBins] = { -0.3, 0.3, 1.0 };
-  const TString etaBinName[nEtaBins] = { "_eastEta", "_midEta", "_westEta" };
-  const TString emw[nEtaBins] = { "east", "mid", "west" };
-  const TString etaBinString[nEtaBins] = { "-0.6<#eta_{jet}<-0.3", "-0.3<#eta_{jet}<0.3", "0.3<#eta_{jet}<0.6" };
-  const int etaColor[nEtaBins] = { 877, 596, 814 };
-
   TLatex *tex = new TLatex( 0.4, 0.95," ");     tex->SetTextFont(63);     tex->SetTextSize(30);
   tex->SetTextColor(kBlack);     tex->SetLineWidth(1);     tex->SetNDC();
 
@@ -66,6 +51,7 @@ int main () {
 
     nAccepted += hResponse[e]->GetEntries();
     nFakes += hFakeJets[e]->GetEntries();
+
   }
 
   TCanvas * can1 = new TCanvas( "can1" , "" ,700 ,500 );              // CANVAS 1
@@ -79,7 +65,7 @@ int main () {
   double scale = (double)nMissed/nEvents;
   hMissedJets->Scale(scale/hMissedJets->Integral());
   hMissedJets->Draw();
-  can2->SaveAs("plots/MissedJets.pdf","PDF");
+  can2->SaveAs("plots/loEA/MissedJets.pdf","PDF");
   
   for (int e=0; e<nEtaBins; ++e) {  
     scale = (double)hFakeJets[e]->GetEntries()/nEvents;
@@ -88,12 +74,12 @@ int main () {
     hFakeJets[e]->SetMarkerColor(etaColor[e]);
     hFakeJets[e]->SetLineColor(etaColor[e]);
     hFakeJets[e]->Draw();
-    name = "plots/FakeJets" + etaBinName[e] + "Jet" + ".pdf";
+    name = "plots/loEA/FakeJets" + etaBinName[e] + "Jet" + ".pdf";
     can2->SaveAs(name,"PDF");
 
     can1->cd();
     hResponse[e]->Draw("COLZ");
-    name = "plots/pTresponse" + etaBinName[e] + "Jet" + ".pdf";
+    name = "plots/loEA/pTresponse" + etaBinName[e] + "Jet" + ".pdf";
     can1->SaveAs(name,"PDF");  // SAVE 2D PT RESPONSE
   }
 
@@ -135,7 +121,7 @@ int main () {
 
     tex->DrawLatex(0.4,0.95,etaBinString[e]);
     
-    name = "plots/detPtResponses" + etaBinName[e] + ".pdf";
+    name = "plots/loEA/detPtResponses" + etaBinName[e] + ".pdf";
     can3->SaveAs(name,"PDF");
 
   }
@@ -158,7 +144,7 @@ int main () {
       if (p==0) { hDetWt[p][e]->Draw(); }
       else { hDetWt[p][e]->Draw("SAME"); }
     }
-    name = "plots/WeightedPtResponse"; name += etaBinName[e] + ".pdf";
+    name = "plots/loEA/WeightedPtResponse"; name += etaBinName[e] + ".pdf";
     can3->BuildLegend(0.65,0.7,0.9,0.9);
     tex->DrawLatex( 0.4, 0.95, etaBinString[e] );
     can3->SaveAs(name,"PDF");
@@ -170,15 +156,15 @@ int main () {
       if (e==0) { hDetWt[p][e]->Draw(); }
       else { hDetWt[p][e]->Draw("SAME"); }
     }
-    name = "plots/WeightedPtResponse"; name += ptBinName[p] + ".pdf";
-    can3->BuildLegend();
+    name = "plots/loEA/WeightedPtResponse"; name += ptBinName[p] + ".pdf";
+    can3->BuildLegend(0.65,0.7,0.9,0.9);
     tex->DrawLatex( 0.4, 0.92, ptBinString[p] );
     can3->SaveAs(name,"PDF");
   }
 
   
   // OPEN pAu FILE AND GATHER HISTOGRAMS OF UE AND LEAD PT
-  TFile *UEfile = new TFile("../out/UE/pAuHTjetUE_diff.root","READ");
+  TFile *UEfile = new TFile("../out/UE/pAuHTjetUE_loEA.root","READ");
 
   const int nEffEtaBins = 20;
   
@@ -196,15 +182,13 @@ int main () {
       hChgUE2D_uncorr[e][f] = (TH2D*)hChgUE3D[e]->Project3D("YX");
       name = "hChgUE2D_te" + etaBinName[e] + "_" + f;
       title = ";leading jet p_{T} (GeV);corrected chg. UE part. p_{T} (GeV)";
-      hChgUE2D_te[e][f] = new TH2D( name, title, 55,4.5,59.5, 30,0.0,30.0 );
+      hChgUE2D_te[e][f] = new TH2D( name, title, 55,4.5,59.5, 15,0.0,15.0 );
     }
     hChgUE3D[e]->GetZaxis()->SetRangeUser(-1.0,1.0);
   }
 
   TH1D *hLeadJetPt[nEtaBins];
   TH2D *hChgUE2D[nEtaBins];
-
-  TH1D *hUEpt[55][nEtaBins];  TH1D *hWtUEpt[nPtBins][nEtaBins];
 
   TCanvas *can4[nEtaBins];
   
@@ -218,19 +202,19 @@ int main () {
     hLeadJetPt[e] = (TH1D*)UEfile->Get(name);
 
     hChgUE2D[e] = (TH2D*)hChgUE3D[e]->Project3D("YX"); // SAVE 2D HISTO OF UE PT vs. LEADING JET PT
-    hChgUE2D[e]->GetXaxis()->SetRangeUser(0.0,60.0);
+    hChgUE2D[e]->GetXaxis()->SetRangeUser(4.5,59.5);
     hChgUE2D[e]->GetYaxis()->SetRangeUser(0.0,15.0);
     hChgUE2D[e]->GetYaxis()->SetTitleOffset(1.25);
     can4[e]->SetLogy(0);
     hChgUE2D[e]->Draw("COLZ");
-    name = "plots/hChgUE2D" + etaBinName[e] + "Jet.pdf";
+    name = "plots/loEA/hChgUE2D" + etaBinName[e] + "Jet.pdf";
     can4[e]->SaveAs(name,"PDF");
     hChgUE2D[e]->GetXaxis()->SetRangeUser(4.5,59.5);
-    hChgUE2D[e]->GetYaxis()->SetRangeUser(0.0,30.0);
-
+    hChgUE2D[e]->GetYaxis()->SetRangeUser(0.0,15.0);
 
   }
 
+  TH1D *hUEpt[55][nEtaBins];  TH1D *hWtUEpt[nPtBins][nEtaBins];
   TCanvas *can5[nEtaBins];
 
   for (int e=0; e<nEtaBins; ++e) {
@@ -242,7 +226,7 @@ int main () {
     for (int p=0; p<nPtBins; ++p) {
       name = "hWtUEpt" + etaBinName[e] + "Jet" + ptBinName[p];
       title = ptBinString[p] + " ~ " + etaBinString[e] + ";ch UE p_{T} (GeV)";
-      hWtUEpt[p][e] = new TH1D(name,title,30,0.0,30.0);
+      hWtUEpt[p][e] = new TH1D(name,title,15,0.0,15.0);
     }
   
     can5[e]->cd();
@@ -252,14 +236,16 @@ int main () {
       int binno = i + 1;
       name = emw[e] + " " + ptVal + " GeV det. jet ";
       hUEpt[i][e] = (TH1D*)hChgUE2D[e]->ProjectionY(name,binno,binno);
+      hUEpt[i][e]->GetXaxis()->SetRangeUser(4.5,59.5);
       if ( (int)hLeadJetPt[e]->GetBinCenter(binno) != ptVal ) { cerr<<"failed pT matching"<<endl; }
-      int nJets = hLeadJetPt[e]->GetBinContent( binno );
-      if (nJets>0){
-	hUEpt[i][e]->Scale(1./nJets);
-	// name = "plots/UEpt" + etaBinName[e] + "_" + ptVal + "GeV" + ".pdf";
+      int n_Jets = hLeadJetPt[e]->GetBinContent( binno );
+      if (n_Jets>0){
+	hUEpt[i][e]->Scale(1./n_Jets);
+	// name = "plots/loEA/UEpt" + etaBinName[e] + "_" + ptVal + "GeV" + ".pdf";
 	// hUEpt[i][e]->Draw();          can5[e]->SaveAs(name,"PDF");
-      
+
 	for (int p=0; p<nPtBins; ++p) {  // ADD UE DISTRIBUTIONS WEIGHTED BY DET-LEVEL FRACTIONAL CONTRIBUTION
+	  if (hDetWt[p][e]->GetBinContent(binno)==0) { continue; }
 	  double wt = hDetWt[p][e]->GetBinContent(binno)*( 1.0 - hFakeJets[e]->GetBinContent(binno) );
 	  hWtUEpt[p][e]->Add( hUEpt[i][e], wt );
 	}
@@ -272,40 +258,33 @@ int main () {
       }
     }
 
-    hUEpt[0][e]->GetYaxis()->SetRangeUser(0.000005,10);
+    hUEpt[0][e]->GetYaxis()->SetRangeUser(0.0000005,2);
     hUEpt[0][e]->Draw("PLC PMC");
 
     for (int i=1; i<55; ++i) { if (hUEpt[i][e]->GetEntries()!=0) { hUEpt[i][e]->Draw("SAME PLC PMC"); } }
     can5[e]->BuildLegend(0.65,0.1,0.9,0.9);
-    name = "plots/detUEpt" + etaBinName[e] + "Jet.pdf";
+    name = "plots/loEA/detUEpt" + etaBinName[e] + "Jet.pdf";
     tex->DrawLatex( 0.4, 0.95, etaBinString[e] );
     can5[e]->SaveAs(name,"PDF");
 
   }
 
   //hChgUE2D_uncorr[e][f]
-
   TH1D *hEffic[nEffEtaBins];
   TFile *ef = new TFile( "src/trackeffic.root", "READ" );
 
-  for (int f=1; f<nEffEtaBins+1; ++f) {
-
-    if (f==21) { continue; }
-
-    cout<<f<<endl;
+  for (int f=1; f<=nEffEtaBins; ++f) {
     
     TString histoName = "eff_s_bin_1_10_bbc__";
     histoName += f; histoName +=  "_"; histoName += f; histoName += "_eta";
     
     hEffic[f] = (TH1D*)ef->Get( histoName );
-
-    //cout<<f<<endl<<histoName<<endl<<hEffic[f]->GetName()<<endl<<endl;
   
-    double pt, effic, ptVal, corrVal, corrErr; // eta, phi, e, px, py, pz, 
+    double pt, effic, ptVal, corrVal, corrErr, jetPt; // eta, phi, e, px, py, pz, 
     int ptBin;
 
     for (int e=0; e<nEtaBins; ++e) { // ~ ~ ~ ~ ~ ~ ~ ~ BEGIN ETA LOOP ~ ~ ~ ~ ~ ~ ~ ~
-
+      
       for ( int iy=1; iy<=hChgUE2D_uncorr[e][f-1]->GetNbinsY(); ++iy ) {  // loop over chg UE pT bins (y)
     	pt = hChgUE2D_uncorr[e][f-1]->GetYaxis()->GetBinCenter(iy);
     	if ( pt > 3.0 ) { pt = 3.0; }
@@ -317,15 +296,12 @@ int main () {
 	  ptVal = hChgUE2D_uncorr[e][f-1]->GetYaxis()->GetBinCenter(iy);
     	  corrVal = hChgUE2D_uncorr[e][f-1]->GetBinContent(ix,iy)/effic;      // calculate corrected bin content and error
     	  corrErr = hChgUE2D_uncorr[e][f-1]->GetBinError(ix,iy)/effic;       // (divide bin content and error by efficiency)
-
-	  double eLo = -1.00 + (f*2.00/nEffEtaBins);
-	  int eval = 99;
-	  for (int e=0; e<nEtaBins; ++e) {    if (eLo>=etaLo[e]) {eval = e;}    }
-	  if (eval==99) { cerr<<"error finding eta bin of histogram!   "<<eLo<<endl; continue; }
-
+	  jetPt = hChgUE2D_uncorr[e][f-1]->GetXaxis()->GetBinCenter(ix);
+	  ptBin = hChgUE2D_uncorr[e][f-1]->GetYaxis()->FindBin( ptVal );
 	  
-    	  hChgUE2D_te[e][f-1]->Fill( ix, ptVal, corrVal );
-    	  hChgUE2D_te[e][f-1]->SetBinError( ix, ptVal, corrErr );
+    	  // hChgUE2D_te[e][f-1]->Fill( jetPt, ptVal, corrVal );
+    	  hChgUE2D_te[e][f-1]->SetBinContent( ix, ptBin, corrVal );
+    	  hChgUE2D_te[e][f-1]->SetBinError( ix, ptBin, corrErr );
 
     	}
       }
@@ -338,67 +314,142 @@ int main () {
 
   TH2D *hChgUE2D_corr[nEffEtaBins];
 
-  // for (int e=0; e<nEtaBins; ++e) {
-  //   name = "hChgUE2D_corr" + etaBinName[e];
-  //   title = ";leading jet p_{T} (GeV);corrected chg. UE part. p_{T} (GeV)";
-  //   hChgUE2D_corr[e] = new TH2D( name, title, 55,4.5,59.5, 30,0.0,30.0 );
-  // }
-  
-  // for (int f=1; f<=nEffEtaBins+1; ++f) {
+  for (int e=0; e<nEtaBins; ++e) {
+    name = "hChgUE2D_corr" + etaBinName[e];
+    title = ";leading jet p_{T} (GeV);corrected chg. UE part. p_{T} (GeV)";
+    hChgUE2D_corr[e] = new TH2D( name, title, 55,4.5,59.5, 15,0.0,15.0 );
+  }
 
-  //   if (f==21) { continue; }
-    
-  //   double eLo = -1.00 + (f*2.00/nEffEtaBins);
-  //   double eHi = eLo + 0.10;
-
-  //   int eval = 99;
-  //   for (int e=0; e<nEtaBins; ++e) {    if (eLo>=etaLo[e]) {eval = e;}    }
-  //   if (eval==99) { cerr<<"error finding eta bin of histogram!   "<<eLo<<endl; continue; }
-
-  //   hChgUE2D_corr[eval]->Add( hChgUE2D_te[eval][f-1] );
-    
-  // }
-
-  // TCanvas * can6 = new TCanvas( "can6" , "" ,700 ,500 );              // CANVAS 6
-  // can6->SetLogz();
-  // for (int e=0; e<nEtaBins; ++e) {
-  //   hChgUE2D_corr[e]->Draw("COLZ");
-  //   name = "plots/ChgUE2D_corr" + etaBinName[e] + ".pdf";
-  //   can6->SaveAs(name,"PDF");
-  // }
-
-
-
-
-  
-  
-  //   hWtUEpt[p]->Draw();
-
-  //   TString text = "#LT p_{T}^{ch}#GT = "; text += hWtUEpt[p]->GetMean(1);
-  //   text = text(0,26);
-
-  //   TLatex *tex = new TLatex( 0.6, 0.55,text);
-  //   tex->SetTextFont(63);
-  //   tex->SetTextSize(20);
-  //   tex->SetTextColor(kBlack);
-  //   tex->SetLineWidth(1);
-  //   tex->SetNDC();
-  //   tex->Draw();
-
-  //   text = "#LT#frac{dN_{ch}}{d#eta d#phi}#GT = "; text += hWtUEpt[p]->Integral()/AREA;
-  //   text = text(0,42);
-  //   tex->DrawLatex( 0.6, 0.4, text );
+  for (int e=0; e<nEtaBins; ++e) {
+    int nHistos = 0;
+    for (int f=0; f<nEffEtaBins; ++f) {
       
-  //   TString name = "plots/wtUEpt" + ptBinName[p] + ".pdf";
-  //   can3->SaveAs(name,"PDF");
-  // }
+      double eLo = -1.00 + (f*2.00/nEffEtaBins);
+      double eHi = eLo + 0.10;
+      
+      int eval = 99;
+      for (int e=0; e<nEtaBins; ++e) {    if (eLo>=etaLo[e]) {eval = e;}    }
+      if (eval==99) { cerr<<"error finding eta bin of histogram!   "<<eLo<<endl; continue; }
+
+      double wt2D = hChgUE2D_te[e][f]->Integral()/hChgUE2D[e]->Integral();
+
+      hChgUE2D_corr[e]->Add( hChgUE2D_te[e][f], wt2D );
+      nHistos +=1;
+      // cout<<etaBinString[e]<<"\t"<<eLo<<"\t"<<eHi<<endl;
+    }
+
+    hChgUE2D_corr[e]->Scale(1./nHistos);
+    // cout<<nHistos<<endl<<endl;
+  }
+  
+  TCanvas * can6 = new TCanvas( "can6" , "" ,700 ,500 );              // CANVAS 6
+  can6->SetLogz();
+  for (int e=0; e<nEtaBins; ++e) {
+    hChgUE2D_corr[e]->Draw("COLZ");
+    name = "plots/loEA/ChgUE2D_corr" + etaBinName[e] + "Jet.pdf";
+    can6->SaveAs(name,"PDF");
+  }
 
 
+  TCanvas * can7[nEtaBins];            // CANVAS 7
+  TH1D *hUEpt_corr[55][nEtaBins];  TH1D *hWtUEpt_corr[nPtBins][nEtaBins];
 
-  // hMissedJets->Draw();
-  // can3->SaveAs("plots/missedJets.pdf","PDF");
-  // hFakeJets->Draw();
-  // can3->SaveAs("plots/fakeJets.pdf","PDF");
+  int nHistos[nPtBins][nEtaBins] = { 0,0,0,0,0,0,0,0,0 };
+
+  double meanSum = 0;
+  
+  for (int e=0; e<nEtaBins; ++e) {
+
+    name = "can7_" + e;
+    can7[e] = new TCanvas( name , "" ,700 ,500 );              // CANVAS 4
+    can7[e]->cd();   can7[e]->SetLogz();   can7[e]->SetLogy(0);
+
+    for (int p=0; p<nPtBins; ++p) {
+      name = "hWtUEpt_corr" + etaBinName[e] + "Jet" + ptBinName[p];
+      title = ptBinString[p] + " ~ " + etaBinString[e] + ";ch UE p_{T} (GeV)";
+      hWtUEpt_corr[p][e] = new TH1D(name,title,15,0.0,15.0);
+    }
+  
+    can7[e]->cd();
+    can7[e]->SetLogy();
+    for (int i=0; i<55; ++i) {  // det-level fractional pT contribution to part-level pT --> fc(pT_det)
+      int ptVal = i + 5;
+      int binno = i + 1;
+      name = emw[e] + " " + ptVal + " GeV det. jet ";
+      hUEpt_corr[i][e] = (TH1D*)hChgUE2D_corr[e]->ProjectionY(name,binno,binno);
+      if ( (int)hLeadJetPt[e]->GetBinCenter(binno) != ptVal ) { cerr<<"failed pT matching"<<endl; }
+      int nJets = hLeadJetPt[e]->GetBinContent( binno );
+      if (nJets>0){
+	hUEpt_corr[i][e]->Scale(1./nJets);
+	// name = "plots/loEA/UEpt" + etaBinName[e] + "_" + ptVal + "GeV" + ".pdf";
+	// hUEpt_corr[i][e]->Draw();          can7[e]->SaveAs(name,"PDF");
+
+	for (int p=0; p<nPtBins; ++p) {  // ADD UE DISTRIBUTIONS WEIGHTED BY DET-LEVEL FRACTIONAL CONTRIBUTION
+	  if (hDetWt[p][e]->GetBinContent(binno)==0) { continue; }
+	  double wt = (hDetWt[p][e]->GetBinContent(binno))*( 1.0 - hFakeJets[e]->GetBinContent(binno) );
+	  hWtUEpt_corr[p][e]->Add( hUEpt_corr[i][e], wt );
+	 
+	  nHistos[p][e] += 1;
+	}
+	// cout<<nHistos<<endl;
+	  
+	hUEpt_corr[i][e]->SetMarkerStyle(marker[i]);
+	hUEpt_corr[i][e]->SetMarkerSize(1);
+	hUEpt_corr[i][e]->SetStats(0);
+	hUEpt_corr[i][e]->SetNameTitle(name,";ch UE p_{T} (GeV)");
+      }
+    }
+
+    hUEpt_corr[0][e]->GetYaxis()->SetRangeUser(0.000005,10);
+    hUEpt_corr[0][e]->Draw("PLC PMC");
+
+    for (int i=1; i<55; ++i) { if (hUEpt_corr[i][e]->GetEntries()!=0) { hUEpt_corr[i][e]->Draw("SAME PLC PMC"); } }
+    can7[e]->BuildLegend(0.65,0.1,0.9,0.9);
+    name = "plots/loEA/detUEpt_corr" + etaBinName[e] + "Jet.pdf";
+    tex->DrawLatex( 0.4, 0.95, etaBinString[e] );
+    can7[e]->SaveAs(name,"PDF");
+
+  }
+
+  for (int p=0; p<nPtBins; ++p) {
+    for (int e=0; e<nEtaBins; ++e) {
+      //hWtUEpt_corr[p][e]->Scale(1./nHistos[p][e]);
+      cout<<hWtUEpt_corr[p][e]->GetMean(1)<<"   "<<hWtUEpt_corr[p][e]->Integral()/AREA<<endl;
+    }
+  }
+  
+  TCanvas * can8 = new TCanvas( "can8" , "" ,700 ,500 );              // CANVAS 8
+  can8->SetLogy();
+  for (int e=0; e<nEtaBins; ++e) {
+    for (int p=0; p<nPtBins; ++p) {
+
+      hWtUEpt_corr[p][e]->Draw();
+
+      TString text = "#LT p_{T}^{ch}#GT = "; text += hWtUEpt_corr[p][e]->GetMean(1);
+      text = text(0,26);
+      
+      TLatex *tex = new TLatex( 0.6, 0.55,text);
+      tex->SetTextFont(63);
+      tex->SetTextSize(20);
+      tex->SetTextColor(kBlack);
+      tex->SetLineWidth(1);
+      tex->SetNDC();
+      tex->Draw();
+      
+      meanSum += hWtUEpt_corr[p][e]->Integral()/AREA;
+      text = "#LT#frac{dN_{ch}}{d#eta d#phi}#GT = "; text += hWtUEpt_corr[p][e]->Integral()/AREA;
+      text = text(0,42);
+      tex->DrawLatex( 0.6, 0.4, text );
+      
+      TString name = "plots/loEA/wtUEpt_corr" + ptBinName[p] + etaBinName[e] + ".pdf";
+      can8->SaveAs(name,"PDF");
+    }
+  }
+
+  cout<<meanSum/9.0<<endl;
+
+  hMissedJets->Draw();
+  can8->SaveAs("plots/loEA/missedJets.pdf","PDF");
 
   
   return 0;
