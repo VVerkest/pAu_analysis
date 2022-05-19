4ffeb0c26cab0cdad4ed7d0e6e34afe5750200ba
Veronica Verkest <vverkest@gmail.com>
2022-05-19 17:49:00 -0400

    move THnSparse binning to parameter header file

--- pAuJetsAndUE/src/pAuHTjetUE.cxx
+++ pAuJetsAndUE/src/pAuHTjetUE.cxx
@@ -23,13 +23,12 @@ int main ( int argc, const char** argv ) {         // funcions and cuts specifie
  [0]: inFile                -> "production_pAu200_2015/HT/pAu_2015_200_HT*.root"
  [1]: outFile               -> "out/UE/pAuHTjetUE.root"
  [2]: number_of_events      -> -1 for all events
+ ...
  [3]: UE_subtraction        -> boolean option for subtracting <rho>A from the jet (to-do)
  [4]: require_dijet         -> boolean option for requiring a dijet (to-do)
- ...
  [5]: trigger_selection     -> TO DO!
  */
     
-  
   vector<string> arguments( argv+1, argv+argc );
   if ( argc ==  6 ) {
       inFile = arguments[0];
@@ -57,20 +56,7 @@ int main ( int argc, const char** argv ) {         // funcions and cuts specifie
       require_dijet = false;
   }
   else { cerr<< "incorrect number of command line arguments"; return 5000; }
-  
-    const int NspJetDims = 6; // leadPt;leadEta;leadPhi;leadNcons;iBBCEsum;zdcx
-    const int NspJetBins[NspJetDims] = { 50, 3, 30, 25, 10, 20 };
-//    spJetBins
-    
-    const double bin_leadPt[NspJetBins[0]+1] = {4., 5., 6., 7., 8., 9., 10., 11., 12., 13., 14., 15., 16., 17., 18., 19., 20., 21., 22., 23., 24., 25., 26., 27., 28., 29., 30., 31., 32., 33., 34., 35., 36., 37., 38., 39., 40., 41., 42., 43., 44., 45., 46., 47., 48., 49., 50., 51., 52., 53., 54.};
-    const double bin_leadEta[NspJetBins[1]+1] = {-1., -0.3, 0.3, 1.};
-    //{-1., -0.9, -0.8, -0.7, -0.6, -0.5, -0.4, -0.3, -0.2, -0.1, 0., 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.};
-    const double bin_leadPhi[NspJetBins[2]+1] = {0., 0.209439, 0.418879, 0.628318, 0.837757, 1.0472, 1.25664, 1.46608, 1.67551, 1.88495, 2.09439, 2.30383, 2.51327, 2.72271, 2.93215, 3.14159, 3.35103, 3.56047, 3.76991, 3.97935, 4.18879, 4.39823, 4.60767, 4.8171, 5.02654, 5.23598, 5.44542, 5.65486, 5.8643, 6.07374, 6.28318};
-    const double bin_leadNcons[NspJetBins[3]+1] = {0., 4., 8., 12., 16., 20., 24., 28., 32., 36., 40., 44., 48., 52., 56., 60., 64., 68., 72., 76., 80., 84., 88., 92., 96., 100.};
-    const double bin_iBBCEsum[NspJetBins[4]+1] = {0., 3559.12, 6735.12, 10126.1, 13752.1, 17669.1, 21948.1, 26718.1, 32283.1, 39473.1, 64000.};
-    const double bin_ZDCx[NspJetBins[5]+1] = {0., 1000., 2000., 3000., 4000., 5000., 6000., 7000., 8000., 9000., 10000., 11000., 12000., 13000., 14000., 15000., 16000., 17000., 18000., 19000., 20000.};
-    //  ZdcCoincidenceRate
-    
+      
     THnSparseD *spJet = new THnSparseD("spJet","Leading Jet;leadPt;leadEta;leadPhi;leadNcons;iBBCEsum;zdcx;",NspJetDims,NspJetBins,NULL,NULL);
     spJet->SetBinEdges( 0, bin_leadPt );
     spJet->SetBinEdges( 1, bin_leadEta );
@@ -78,13 +64,6 @@ int main ( int argc, const char** argv ) {         // funcions and cuts specifie
     spJet->SetBinEdges( 3, bin_leadNcons );
     spJet->SetBinEdges( 4, bin_iBBCEsum );
     spJet->SetBinEdges( 5, bin_ZDCx );
-
-    const int NspUEdims = 6;
-    const int NspUEbins[NspUEdims] = { 50, 15, 20, 30, 20, 10 };
-    
-    const double bin_UEpt[NspUEbins[1]+1] = { 0.20, 0.25, 0.30, 0.35, 0.40, 0.50, 0.60, 0.70, 0.80, 1.0, 1.5, 2.0, 3.0, 5.0, 10.0, 15.0 };
-    const double bin_UEeta[NspUEbins[2]+1] = {-1., -0.9, -0.8, -0.7, -0.6, -0.5, -0.4, -0.3, -0.2, -0.1, 0., 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.};
-    const double bin_UEphi[NspUEbins[3]+1] = {0., 0.209439, 0.418879, 0.628318, 0.837757, 1.0472, 1.25664, 1.46608, 1.67551, 1.88495, 2.09439, 2.30383, 2.51327, 2.72271, 2.93215, 3.14159, 3.35103, 3.56047, 3.76991, 3.97935, 4.18879, 4.39823, 4.60767, 4.8171, 5.02654, 5.23598, 5.44542, 5.65486, 5.8643, 6.07374, 6.28318};
     
     THnSparseD *spUE = new THnSparseD("spUE","Underlying Event;leadPt;UEpt;UEeta;UEphi;leadEta;iBBCEsum;",NspUEdims,NspUEbins,NULL,NULL);
     spUE->SetBinEdges( 0, bin_leadPt );
@@ -94,28 +73,24 @@ int main ( int argc, const char** argv ) {         // funcions and cuts specifie
     spUE->SetBinEdges( 4, bin_leadEta );
     spUE->SetBinEdges( 5, bin_iBBCEsum );
 
-    
-  
-  JetDefinition jet_def(antikt_algorithm, R);     //  JET DEFINITION
-  Selector jetEtaSelector = SelectorAbsEtaMax( 1.0-R );
-  Selector leadPtMinSelector = SelectorPtMin(leadJetMinPt);
-  Selector leadJetSelector = jetEtaSelector;// && leadPtMinSelector;
-  Selector ptMaxSelector = SelectorPtMax( 30.0 );
+    JetDefinition jet_def(antikt_algorithm, R);     //  JET DEFINITION
+    Selector jetEtaSelector = SelectorAbsEtaMax( 1.0-R );
+    Selector leadPtMinSelector = SelectorPtMin(leadJetMinPt);
+    Selector leadJetSelector = jetEtaSelector;// && leadPtMinSelector;
+    Selector ptMaxSelector = SelectorPtMax( 30.0 );
 
-  TStarJetPicoEventHeader* header;
-  TStarJetPicoEvent* event;
-  TStarJetVectorContainer<TStarJetVector> * container;
+    TStarJetPicoEventHeader* header;
+    TStarJetPicoEvent* event;
+    TStarJetVectorContainer<TStarJetVector> * container;
 
-  TChain* Chain = new TChain( "JetTree" );
-  Chain->Add( inFile.c_str() );
-  TStarJetPicoReader Reader;
-  InitReader( Reader, Chain, number_of_events );
-
+    TChain* Chain = new TChain( "JetTree" );
+    Chain->Add( inFile.c_str() );
+    TStarJetPicoReader Reader;
+    InitReader( Reader, Chain, number_of_events );
 
 //  string efficFile = "src/trackeffic.root";
 //  string UEcorrFile = "src/UEsubtractionPlots.root";
 
-    //  Tree variables
     int RunID, EventID, nTowers, nPrimary, nGlobal, nVertices, refMult, gRefMult, nUEpart_chg, nUEpart_neu, nJetsAbove5, nHTtrig;
     double Vz, vpdVz, BbcAdcSumEast, leadPt, leadPtCorrected, leadEta, leadPhi, chgEastRho, chgMidRho, chgWestRho, chgEastRho_te,
       chgMidRho_te, chgWestRho_te, neuEastRho, neuMidRho, neuWestRho, leadArea, dPhiTrigLead, dRTrigLead, trigPhi, zdcx, nCons;
@@ -127,8 +102,7 @@ int main ( int argc, const char** argv ) {         // funcions and cuts specifie
     vector<PseudoJet> rawParticles, rawJets, allJets, chgParticles, neuParticles, UEparticles, cons;
 
     
-    
-  // ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~  BEGIN EVENT LOOP!  ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+    // ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~  BEGIN EVENT LOOP!  ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
   while ( Reader.NextEvent() ) {
     Reader.PrintStatus(10);
     rawParticles.clear();    chgParticles.clear();    neuParticles.clear();    rawJets.clear();    allJets.clear();    //  CLEAR VECTORS
@@ -141,13 +115,7 @@ int main ( int argc, const char** argv ) {         // funcions and cuts specifie
     if ( abs(Vz) > vzCut ) { continue; }
     if (!(header->HasTriggerId(500205) || header->HasTriggerId(500215))) {continue;}   //  ONLY SELECT HT TRIGGER EVENTS
     if ( header->GetBbcAdcSumEast() > 64000 ) { continue; }
-    if ( header->GetBbcAdcSumEast() < 3559.12 ) { continue; }     //  neglect 90-100% event activity
-
-    // //  HIGH EVENT ACTIVITY
-    // if ( header->GetBbcAdcSumEast() < 26718.1 ) { continue; }  // LO: 3559.12-10126.1;  HI: 26718.1+// 
-
-    // //  LOW EVENT ACTIVITY
-    // if ( header->GetBbcAdcSumEast() > 10126.1 ) { continue; }  // LO: 3559.12-10126.1;  HI: 26718.1+
+//    if ( header->GetBbcAdcSumEast() < 3559.12 ) { continue; }     //  neglect 90-100% event activity
     
     TList *SelectedTowers = Reader.GetListOfSelectedTowers();
     nTowers = CountTowers( SelectedTowers );
@@ -201,47 +169,32 @@ int main ( int argc, const char** argv ) {         // funcions and cuts specifie
 	  Selector allJetSelector = SelectorPtMin(2.0) && ptMaxSelector && jetEtaSelector;
 	  allJets = sorted_by_pt( allJetSelector( jetCluster.inclusive_jets() ) );     // EXTRACT ALL JETS IN >=5 GeV
 	  
-//	  nJetsAbove5 = allJets.size();
-//
-//	  vpdVz = header->GetvpdVz();
-//	  RunID = header->GetRunId();
-//	  EventID = Reader.GetNOfCurrentEvent();
-//	  nPrimary = header->GetNOfPrimaryTracks();
-//	  nGlobal = header->GetNGlobalTracks();
-//	  nVertices = header->GetNumberOfVertices();
-//	  refMult = header->GetReferenceMultiplicity();
-//	  gRefMult = header->GetGReferenceMultiplicity();
 	  BbcAdcSumEast = header->GetBbcAdcSumEast();
 	  leadPt = leadJet.pt();
 //	  leadPtCorrected = UEsubtraction( leadJet, UEcorrFile, BbcAdcSumEast );
 	  leadEta = leadJet.eta();
 	  leadPhi = leadJet.phi();
 //	  leadArea = leadJet.area();
-        zdcx = header->GetZdcCoincidenceRate();
+      zdcx = header->GetZdcCoincidenceRate();
         
 	  //  UNDERLYING EVENT ESTIMATION
-	  // GatherChargedUEwithEfficiency( leadJet, container, chgParticles, efficFile );
 	  GatherChargedUE( leadJet, container, chgParticles );
 	  GatherNeutralUE( leadJet, container, neuParticles );
-
 	  nUEpart_chg = chgParticles.size();
 	  nUEpart_neu = neuParticles.size();
-    
-	  chgEastSum = 0;  chgMidSum = 0;  chgWestSum = 0;  neuEastSum = 0;  neuMidSum = 0;  neuWestSum = 0;
-	  CalculateRhoByChargeAndEta(chgParticles,neuParticles,chgEastSum,chgMidSum,chgWestSum,neuEastSum,neuMidSum,neuWestSum);
-	  chgEastRho = chgEastSum/eastArea;
-	  chgMidRho = chgMidSum/midArea;
-	  chgWestRho = chgWestSum/westArea;
-	  neuEastRho = neuEastSum/eastArea;
-	  neuMidRho = neuMidSum/midArea;
-	  neuWestRho = neuWestSum/westArea;
+//	  chgEastSum = 0;  chgMidSum = 0;  chgWestSum = 0;  neuEastSum = 0;  neuMidSum = 0;  neuWestSum = 0;
+//	  CalculateRhoByChargeAndEta(chgParticles,neuParticles,chgEastSum,chgMidSum,chgWestSum,neuEastSum,neuMidSum,neuWestSum);
+//	  chgEastRho = chgEastSum/eastArea;
+//	  chgMidRho = chgMidSum/midArea;
+//	  chgWestRho = chgWestSum/westArea;
+//	  neuEastRho = neuEastSum/eastArea;
+//	  neuMidRho = neuMidSum/midArea;
+//	  neuWestRho = neuWestSum/westArea;
 	  
 	  pval = 99;    jeval = 99;
-
 	  for ( int e=0; e<nEtaBins; ++e ) {
 	    if ( leadEta >= etaLo[e]  &&  leadEta <= etaHi[e] ) { jeval = e; }
 	  }
-	  
 	  if ( jeval==99 ) { cerr<<"UNABLE TO FIND PT OR ETA RANGE FOR LEAD JET"<<endl/*<<leadPt<<endl<<endl*/; }
 
 	  
@@ -249,57 +202,28 @@ int main ( int argc, const char** argv ) {         // funcions and cuts specifie
 	  
         cons = leadJet.constituents();
         nCons = cons.size();
-//        cout<<"..."<<nCons<<"..."<<endl;
         // spJet: leadPt;leadEta;leadPhi;leadNcons;iBBCEsum;zdcx
         double jetFill[NspJetDims+1] = {  leadPt, leadEta, leadPhi, nCons, BbcAdcSumEast, zdcx };
         spJet->Fill( jetFill );
-
         
 	  for (int i=0; i<chgParticles.size(); ++i) {
-          
           // spUE: leadPt;UEpt;UEeta;UEphi;leadEta;iBBCEsum
           double UEfill[NspUEdims+1] = {  leadPt, chgParticles[i].pt(), chgParticles[i].eta(), chgParticles[i].phi(), leadEta, BbcAdcSumEast };
           spUE->Fill( UEfill );
-          
-//	    hChgUE[jeval]->Fill( leadPt, chgParticles[i].pt(), chgParticles[i].eta() );
-//	    hLeadPtVsUEpT->Fill( leadPt, chgParticles[i].pt());
-	    // hChgUE[jeval]->Fill( leadPtCorrected, chgParticles[i].pt(), chgParticles[i].eta() );
-	    // hLeadPtVsUEpT->Fill( leadPtCorrected, chgParticles[i].pt());
 	  }
-	  // hLeadPt[jeval]->Fill( leadPtCorrected );
-	  // hLead->Fill( leadPtCorrected );
-//	  hLeadPt[jeval]->Fill( leadPt );
-//	  hLead->Fill( leadPt );
 
 	  chgParticles.clear(); // clear vector!
 
-//	  CalculateUEsubtractedChargedRho(chgParticles,chgEastSum,chgMidSum,chgWestSum);
-//	  chgEastRho_te = chgEastSum/eastArea;
-//	  chgMidRho_te = chgMidSum/midArea;
-//	  chgWestRho_te = chgWestSum/westArea;
-
-	  
-//	  HTjetTree->Fill();
         }
       }
     }
   }  // ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~  END EVENT LOOP!  ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
-
     
     
 
     TFile *pAuFile = new TFile( outFile.c_str() ,"RECREATE");
     spJet->Write();
     spUE->Write();
-    
-//  for ( int e=0; e<nEtaBins; ++e ) {  //  WRITE HISTOGRAMS & TREE
-//    hChgUE[e]->Write();
-//    hLeadPt[e]->Write();
-//  }
-//  HTjetTree->Write();
-//
-//  hLeadPtVsUEpT->Write();
-//  hLead->Write();
   
   pAuFile->Write();
   pAuFile->Close();
